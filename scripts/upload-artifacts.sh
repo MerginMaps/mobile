beebbc1a10f5fb5308560f3325aaea1da4d9e9d0
Peter Petrik <zilolv@gmail.com>
2019-04-02 15:24:20 +0200

    upload APK to dropbox

---
+++ scripts/upload-artifacts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+export UPLOAD_ARTIFACT_ID=$( [[ ${TRAVIS_PULL_REQUEST} =~ false ]] && echo ${TRAVIS_TAG} || echo ${TRAVIS_PULL_REQUEST} )
+export APK_FILE=input-${UPLOAD_ARTIFACT_ID}-${TRAVIS_COMMIT}-${ARCH}.apk
+
+# If we have secure env vars and are in either a pull request or a tag, we need to upload artifacts
+if [[ "${TRAVIS_SECURE_ENV_VARS}" = "true" ]];
+then
+  if [ ${TRAVIS_PULL_REQUEST} != false ]; then
+    export DROPBOX_FOLDER="pulls"
+  elif [[ -n ${TRAVIS_TAG} ]]; then
+    export DROPBOX_FOLDER="tags"
+  elif [[ ${TRAVIS_BRANCH} = master ]]; then
+    export DROPBOX_FOLDER="master"
+  fi
+
+  sudo cp build-${ARCH}/out/build/outputs/apk/release/out-release-signed.apk /tmp/${APK_FILE}
+  python3 uploader.py --source /tmp/${APK_FILE} --destination "$DROPBOX_FOLDER/${APK_FILE}" --token DROPBOX_TOKEN
+  
+else
+  echo -e "Not uploading artifacts ..."
+  if [ "${TRAVIS_SECURE_ENV_VARS}" != "true" ];
+  then
+    echo -e "  TRAVIS_SECURE_ENV_VARS is not true (${TRAVIS_SECURE_ENV_VARS})"
+  fi
+  if [ "${TRAVIS_SECURE_ENV_VARS}" = "false" ];
+  then
+    echo -e "  TRAVIS_PULL_REQUEST is false (${TRAVIS_PULL_REQUEST})"
+  fi
+  if [ "${TRAVIS_TAG}X" = "X" ];
+  then
+    echo -e "  TRAVIS_TAG is not set"
+  fi
+fi
---
+++ scripts/uploader.py
@@ -0,0 +1,94 @@
+# 2018 Peter Petrik (zilolv at gmail dot com)
+# GNU General Public License 2 any later version
+
+import argparse
+import os
+import dropbox
+from dropbox.files import WriteMode
+from dropbox.exceptions import ApiError, AuthError
+
+
+# inspired by
+# https://github.com/dropbox/dropbox-sdk-python/blob/master/example/back-up-and-restore/backup-and-restore-example.py
+class UploadError(Exception):
+    pass
+
+
+# Uploads contents of LOCALFILE to Dropbox
+# from Dropbox's examples
+# files < 150 MB, see https://stackoverflow.com/q/33810138/2838364
+# files larger > 150 MB https://stackoverflow.com/a/40114617/2838364
+def backup(local, remote):
+    file_size = os.path.getsize(local)
+    CHUNK_SIZE = 4 * 1024 * 1024
+
+    with open(local, 'rb') as f:
+        # We use WriteMode=overwrite to make sure that the settings in the file are changed on upload
+        try:
+            if file_size <= CHUNK_SIZE:
+                dbx.files_upload(f.read(), remote, mode=WriteMode('overwrite'))
+            else:
+                upload_session_start_result = dbx.files_upload_session_start(f.read(CHUNK_SIZE))
+                cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,
+                                                           offset=f.tell())
+                commit = dropbox.files.CommitInfo(path=remote)
+
+                while f.tell() < file_size:
+                    if (file_size - f.tell()) <= CHUNK_SIZE:
+                        dbx.files_upload_session_finish(f.read(CHUNK_SIZE),
+                                                        cursor,
+                                                        commit)
+                    else:
+                        dbx.files_upload_session_append_v2(f.read(CHUNK_SIZE),
+                                                           cursor)
+                        cursor.offset = f.tell()
+
+        except ApiError as err:
+            # This checks for the specific error where a user doesn't have
+            # enough Dropbox space quota to upload this file
+            if (err.error.is_path() and
+                    err.error.get_path().reason.is_insufficient_space()):
+                raise UploadError("ERROR: Cannot back up; insufficient space.")
+            elif err.user_message_text:
+                raise UploadError(err.user_message_text)
+            else:
+                raise UploadError(str(err))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Upload QGIS package to store')
+    parser.add_argument('--source',
+                        required=True,
+                        help='file to upload')
+    parser.add_argument('--destination',
+                        required=True,
+                        help='full filename on Dropbox where to upload file')
+    parser.add_argument('--token',
+                        required=True,
+                        help='Dropbox APIv2 Token key in environment')
+
+    args = parser.parse_args()
+    if not os.path.exists(args.source):
+        raise UploadError(args.source + " does not exists")
+
+    # parse token
+    token = args.token
+    token = os.environ.get[token] # DO NOT print this value, it is SECRET on travis!
+    if len(token) != 64:
+        raise UploadError("ERROR: Looks like your Dropbox access token is not valid, should be 64 char long")
+
+    # Create an instance of a Dropbox class, which can make requests to the API.
+    dbx = dropbox.Dropbox(token)
+
+    # Check that the access token is valid
+    try:
+        dbx.users_get_current_account()
+    except AuthError as err:
+        raise UploadError("ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
+
+    # Create a backup of the current settings file
+    backup(local=args.source, remote=args.destination)
+
+    # Share for public
+    r = dbx.sharing_create_shared_link_with_settings(args.destination)
+    print("Upload " + args.source + " to " + str(r.url) + "(" + args.destination + ")")
