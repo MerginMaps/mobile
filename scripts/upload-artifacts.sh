189e5abf7e40206042921bba10b0b4eeb754ddc8
Peter Petrik <zilolv@gmail.com>
2019-04-03 10:34:12 +0200

    another try

--- scripts/upload-artifacts.sh
+++ scripts/upload-artifacts.sh
@@ -2,7 +2,7 @@
 set -e
 
 # export BUILD_FILE=/usr/src/input/build-${ARCH}/out/build/outputs/apk/release/out-release-signed.apk
-export BUILD_FILE=build-${ARCH}/out//build/outputs/apk/debug/out-debug.apk
+export BUILD_FILE=build-${ARCH}/out/build/outputs/apk/debug/out-debug.apk
 export GITHUB_REPO=lutraconsulting/input
 
 # If we have secure env vars and are in either a pull request or a tag, we need to upload artifacts
@@ -24,13 +24,15 @@ then
     export APK_FILE=input-${TRAVIS_BRANCH}-${TRAVIS_COMMIT}-${ARCH}.apk
     export GITHUB_API=https://api.github.com/repos/${GITHUB_REPO}/commits/${TRAVIS_COMMIT}/comments
   else
-    echo "Nothing to do, not PR, master or tagged release"
-    exit 0
+    echo -e "\e[31mDeploying other commit\e[0m"
+    export DROPBOX_FOLDER="other"
+    export APK_FILE=input-${TRAVIS_BRANCH}-${TRAVIS_COMMIT}-${ARCH}.apk
+    export GITHUB_API=https://api.github.com/repos/${GITHUB_REPO}/commits/${TRAVIS_COMMIT}/comments
   fi
 
   sudo cp ${BUILD_FILE} /tmp/${APK_FILE}
   APK_URL=`python3 ./scripts/uploader.py --source /tmp/${APK_FILE} --destination "$DROPBOX_FOLDER/${APK_FILE}" --token DROPBOX_TOKEN | tail -n 1`
-  curl -u inputapp-bot:${GITHUB_TOKEN} -X POST --data '{"body": "Apk ready for [armv7](${APK_URL})"}' https://api.github.com/repos/${GITHUB_REPO}/issues/${TRAVIS_PULL_REQUEST}/comments
+  curl -u inputapp-bot:${GITHUB_TOKEN} -X POST --data '{"body": "Apk: [armv7](${APK_URL}) (SDK: ${OSGEO4A_SDK})"}' https://api.github.com/repos/${GITHUB_REPO}/issues/${TRAVIS_PULL_REQUEST}/comments
 
 else
   echo -e "Not uploading artifacts ..."
