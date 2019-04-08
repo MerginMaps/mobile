8d89b37df03a459d72f9a44a4af5fd178e23df42
Matthias Kuhn <matthias@opengis.ch>
2019-04-08 21:15:50 +0200

    Code formatting

--- app/merginapi.h
+++ app/merginapi.h
@@ -16,34 +16,37 @@ enum ProjectStatus
 };
 Q_ENUMS( ProjectStatus )
 
-struct MerginProject {
-    QString name;
-    QStringList tags;
-    QDateTime created;
-    QDateTime updated; // local version of project files
-    QDateTime serverUpdated; // available version of project files on server
-    QDateTime lastSync; // local datetime of download/upload/update project
-    bool pending = false; // if there is a pending request for downlaod/update a project
-    ProjectStatus status = NoVersion;
-    int size;
-    int filesCount;
+struct MerginProject
+{
+  QString name;
+  QStringList tags;
+  QDateTime created;
+  QDateTime updated; // local version of project files
+  QDateTime serverUpdated; // available version of project files on server
+  QDateTime lastSync; // local datetime of download/upload/update project
+  bool pending = false; // if there is a pending request for downlaod/update a project
+  ProjectStatus status = NoVersion;
+  int size;
+  int filesCount;
 };
 
-struct MerginFile {
-    QString path;
-    QString checksum;
-    qint64 size;
+struct MerginFile
+{
+  QString path;
+  QString checksum;
+  qint64 size;
 };
 
 
 typedef QList<std::shared_ptr<MerginProject>> ProjectList;
 
-class MerginApi: public QObject {
+class MerginApi: public QObject
+{
     Q_OBJECT
-    Q_PROPERTY(QString username READ username NOTIFY authChanged)
-     Q_PROPERTY(QString apiRoot READ apiRoot WRITE setApiRoot NOTIFY apiRootChanged)
-public:
-    explicit MerginApi(const QString& dataDir, QObject* parent = nullptr );
+    Q_PROPERTY( QString username READ username NOTIFY authChanged )
+    Q_PROPERTY( QString apiRoot READ apiRoot WRITE setApiRoot NOTIFY apiRootChanged )
+  public:
+    explicit MerginApi( const QString &dataDir, QObject *parent = nullptr );
     ~MerginApi() = default;
 
     /**
@@ -53,7 +56,7 @@ public:
      * If listing has been successful, updates cached merginProjects list.
      * @param withFilter If true, applies "input" tag in request.
      */
-    Q_INVOKABLE void listProjects(QString filterTag = QStringLiteral("input_use"));
+    Q_INVOKABLE void listProjects( QString filterTag = QStringLiteral( "input_use" ) );
 
     /**
      * Sends non-blocking GET request to the server to download a project with a given name. On downloadProjectReplyFinished,
@@ -63,7 +66,7 @@ public:
      * Emits also notify signal with a message for the GUI.
      * @param projectName Name of project to download.
      */
-    Q_INVOKABLE void downloadProject(QString projectName);
+    Q_INVOKABLE void downloadProject( QString projectName );
 
     /**
      * Sends non-blocking POST request to the server to update a project with a given name. On downloadProjectReplyFinished,
@@ -73,7 +76,7 @@ public:
      * Emits also notify signal with a message for the GUI.
      * @param projectName Name of project to update.
      */
-    Q_INVOKABLE void updateProject(QString projectName);
+    Q_INVOKABLE void updateProject( QString projectName );
 
     /**
      * Sends non-blocking POST request to the server to upload changes in a project with a given name.
@@ -82,75 +85,75 @@ public:
      * Emits also notify signal with a message for the GUI.
      * @param projectName Name of project to upload.
      */
-    Q_INVOKABLE void uploadProject(QString projectName);
+    Q_INVOKABLE void uploadProject( QString projectName );
 
-     /**
-     * Currently no auth service is used, only "username:password" is encoded and asign to mToken.
-     * @param username
-     * @param password
-     */
-    Q_INVOKABLE void authorize(QString username, QString password);
+    /**
+    * Currently no auth service is used, only "username:password" is encoded and asign to mToken.
+    * @param username
+    * @param password
+    */
+    Q_INVOKABLE void authorize( QString username, QString password );
     Q_INVOKABLE void clearAuth();
     Q_INVOKABLE void resetApiRoot();
     Q_INVOKABLE bool hasAuthData();
 
     // Test functions
-    void createProject(QString projectName);
-    void deleteProject(QString projectName);
+    void createProject( QString projectName );
+    void deleteProject( QString projectName );
 
     ProjectList projects();
 
     QString username() const;
 
     QString apiRoot() const;
-    void setApiRoot(const QString &apiRoot);
+    void setApiRoot( const QString &apiRoot );
 
-signals:
-    void listProjectsFinished(ProjectList merginProjects);
-    void syncProjectFinished(QString projectDir, QString projectName, bool successfully = true);
-    void reloadProject(QString projectDir);
-    void networkErrorOccurred(QString message, QString additionalInfo);
-    void notify(QString message);
+  signals:
+    void listProjectsFinished( ProjectList merginProjects );
+    void syncProjectFinished( QString projectDir, QString projectName, bool successfully = true );
+    void reloadProject( QString projectDir );
+    void networkErrorOccurred( QString message, QString additionalInfo );
+    void notify( QString message );
     void merginProjectsChanged();
     void authRequested();
     void authChanged();
     void authFailed();
     void apiRootChanged();
-    void projectCreated(QString projectName);
-    void serverProjectDeleted(QString projectName);
+    void projectCreated( QString projectName );
+    void serverProjectDeleted( QString projectName );
 
-public slots:
-    void projectDeleted(QString projectName);
+  public slots:
+    void projectDeleted( QString projectName );
 
-private slots:
+  private slots:
     void listProjectsReplyFinished();
     void downloadProjectReplyFinished(); // download + update
     void uploadProjectReplyFinished();
     void updateInfoReplyFinished();
     void uploadInfoReplyFinished();
     void cacheProjects();
-    void continueWithUpload(QString projectDir, QString projectName, bool successfully = true);
-    void setUpdateToProject(QString projectDir, QString projectName, bool successfully);
+    void continueWithUpload( QString projectDir, QString projectName, bool successfully = true );
+    void setUpdateToProject( QString projectDir, QString projectName, bool successfully );
     void saveAuthData();
     void createProjectFinished();
     void deleteProjectFinished();
     void authorizeFinished();
 
 
-private:
-    ProjectList parseProjectsData(const QByteArray &data, bool dataFromServer = false);
-    bool cacheProjectsData(const QByteArray &data);
-    void handleDataStream(QNetworkReply* r, QString projectDir, bool overwrite);
-    bool saveFile(const QByteArray &data, QFile &file, bool closeFile);
-    void createPathIfNotExists(QString filePath);
-    ProjectStatus getProjectStatus(QDateTime localUpdated, QDateTime updated, QDateTime lastSync, QDateTime lastMod);
-    QByteArray getChecksum(QString filePath);
-    QSet<QString> listFiles(QString projectPath);
-    void downloadProjectFiles(QString projectName, QByteArray json);
-    void uploadProjectFiles(QString projectName, QByteArray json, QList<MerginFile> files);
-    QHash<QString, QList<MerginFile>> parseAndCompareProjectFiles(QNetworkReply *r, bool isForUpdate);
-    ProjectList updateMerginProjectList(ProjectList serverProjects);
-    void deleteObsoleteFiles(QString projectName);
+  private:
+    ProjectList parseProjectsData( const QByteArray &data, bool dataFromServer = false );
+    bool cacheProjectsData( const QByteArray &data );
+    void handleDataStream( QNetworkReply *r, QString projectDir, bool overwrite );
+    bool saveFile( const QByteArray &data, QFile &file, bool closeFile );
+    void createPathIfNotExists( QString filePath );
+    ProjectStatus getProjectStatus( QDateTime localUpdated, QDateTime updated, QDateTime lastSync, QDateTime lastMod );
+    QByteArray getChecksum( QString filePath );
+    QSet<QString> listFiles( QString projectPath );
+    void downloadProjectFiles( QString projectName, QByteArray json );
+    void uploadProjectFiles( QString projectName, QByteArray json, QList<MerginFile> files );
+    QHash<QString, QList<MerginFile>> parseAndCompareProjectFiles( QNetworkReply *r, bool isForUpdate );
+    ProjectList updateMerginProjectList( ProjectList serverProjects );
+    void deleteObsoleteFiles( QString projectName );
     QByteArray generateToken();
     void loadAuthData();
     static QString defaultApiRoot() { return "https://public.cloudmergin.com/"; }
--- app/merginprojectmodel.cpp
+++ app/merginprojectmodel.cpp
@@ -3,90 +3,108 @@
 #include <QAbstractListModel>
 #include <QString>
 
-MerginProjectModel::MerginProjectModel(QObject* parent)
-    : QAbstractListModel( parent )
+MerginProjectModel::MerginProjectModel( QObject *parent )
+  : QAbstractListModel( parent )
 {
 }
 
-QVariant MerginProjectModel::data( const QModelIndex& index, int role ) const
+QVariant MerginProjectModel::data( const QModelIndex &index, int role ) const
 {
-    int row = index.row();
-    if (row < 0 || row >= mMerginProjects.count())
-        return QVariant();
-
-    const MerginProject* project = mMerginProjects.at(row).get();
-
-    switch ( role )
-    {
-    case Name: return QVariant(project->name);
-    case ProjectInfo: {
-        if (!project->updated.isValid()) {
-            return QVariant(project->serverUpdated).toString();
-        } else {
-            return QVariant(project->updated).toString();
-        }
-    }
-    case Status: {
-        if (project->status == ProjectStatus::OutOfDate) return QVariant("outOfDate");
-        if (project->status == ProjectStatus::UpToDate) return QVariant("upToDate");
-        if (project->status == ProjectStatus::NoVersion) return QVariant("noVersion");
-        if (project->status == ProjectStatus::Modified) return QVariant("modified");
-        return QVariant("noVersion");
-    }
-    case Pending: return QVariant(project->pending);
-    }
-
+  int row = index.row();
+  if ( row < 0 || row >= mMerginProjects.count() )
     return QVariant();
+
+  const MerginProject *project = mMerginProjects.at( row ).get();
+
+  switch ( role )
+  {
+    case Name:
+      return QVariant( project->name );
+
+    case ProjectInfo:
+    {
+      if ( !project->updated.isValid() )
+      {
+        return QVariant( project->serverUpdated ).toString();
+      }
+      else
+      {
+        return QVariant( project->updated ).toString();
+      }
+    }
+
+    case Status:
+    {
+      switch ( project->status )
+      {
+        case ProjectStatus::OutOfDate:
+          return QVariant( QStringLiteral("outOfDate") );
+        case ProjectStatus::UpToDate:
+          return QVariant( QStringLiteral("upToDate") );
+        case ProjectStatus::NoVersion:
+          return QVariant( QStringLiteral("noVersion") );
+        case ProjectStatus::Modified:
+          return QVariant( QStringLiteral("modified") );
+      }
+    }
+    case Pending: return QVariant( project->pending );
+  }
+
+  return QVariant();
 }
 
-void MerginProjectModel::setPending(int row, bool pending)
+void MerginProjectModel::setPending( int row, bool pending )
 {
-    if (row < 0 || row > mMerginProjects.length() - 1) return;
-    QModelIndex ix = index(row);
-    mMerginProjects.at(row)->pending = pending;
-    emit dataChanged(ix, ix);
+  if ( row < 0 || row > mMerginProjects.length() - 1 ) return;
+  QModelIndex ix = index( row );
+  mMerginProjects.at( row )->pending = pending;
+  emit dataChanged( ix, ix );
 }
 
 QHash<int, QByteArray> MerginProjectModel::roleNames() const
 {
-    QHash<int, QByteArray> roleNames = QAbstractListModel::roleNames();
-    roleNames[Name] = "name";
-    roleNames[ProjectInfo] = "projectInfo";
-    roleNames[Status] = "status";
-    roleNames[Pending] = "pendingProject";
-    return roleNames;
+  QHash<int, QByteArray> roleNames = QAbstractListModel::roleNames();
+  roleNames[Name] = "name";
+  roleNames[ProjectInfo] = "projectInfo";
+  roleNames[Status] = "status";
+  roleNames[Pending] = "pendingProject";
+  return roleNames;
 }
 
 ProjectList MerginProjectModel::projects()
 {
-    return mMerginProjects;
+  return mMerginProjects;
 }
 
-int MerginProjectModel::rowCount(const QModelIndex &parent) const {
-    Q_UNUSED(parent);
-    return mMerginProjects.count();
-}
-
-void MerginProjectModel::resetProjects(const ProjectList &merginProjects)
+int MerginProjectModel::rowCount( const QModelIndex &parent ) const
 {
-    mMerginProjects.clear();
-    beginResetModel();
-    mMerginProjects = merginProjects;
-    endResetModel();
+  Q_UNUSED( parent );
+  return mMerginProjects.count();
 }
 
-void MerginProjectModel::syncProjectFinished(QString projectFolder, QString projectName, bool successfully)
+void MerginProjectModel::resetProjects( const ProjectList &merginProjects )
 {
-    Q_UNUSED(projectFolder);
-    int row = 0;
-    for (std::shared_ptr<MerginProject> project: mMerginProjects) {
-        if (project->name == projectName) {
-            if (successfully) {
-                project->status = ProjectStatus::UpToDate;
-            }
-            setPending(row, false); // emits dataChanged
-            return;
-        }
-        row++;
+  mMerginProjects.clear();
+  beginResetModel();
+  mMerginProjects = merginProjects;
+  endResetModel();
+}
+
+void MerginProjectModel::syncProjectFinished( QString projectFolder, QString projectName, bool successfully )
+{
+  Q_UNUSED( projectFolder );
+  int row = 0;
+  for ( std::shared_ptr<MerginProject> project : mMerginProjects )
+  {
+    if ( project->name == projectName )
+    {
+      if ( successfully )
+      {
+        project->status = ProjectStatus::UpToDate;
+      }
+      setPending( row, false ); // emits dataChanged
+      return;
     }
+    row++;
+  }
 }
