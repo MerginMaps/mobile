4c7d0effe861c4017d87c8bb3c6579d28f5df385
Peter Petrik <zilolv@gmail.com>
2019-04-09 08:18:47 +0200

    install astyle

--- scripts/check_all.bash
+++ scripts/check_all.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PWD=`pwd`
 cd $DIR
-CI=0 ./astyle.bash `find .. -name \*.h* -print -o -name \*.c* -print`
+./astyle.bash `find .. -name \*.h* -print -o -name \*.c* -print`
 cd $PWD
