b1cac96f7b3d8f1052afc7242f7cd1ad1d34e225
Peter Petrik <zilolv@gmail.com>
2019-11-18 11:26:08 +0100

    fix win vars

--- scripts/download_sdk.cmd
+++ scripts/download_sdk.cmd
@@ -1,5 +1,6 @@
 @echo on
 set INPUT_SDK_DIR=C:\projects\input-sdk\x86_64\stage
+mkdir %INPUT_SDK_DIR%
 
 curl -fsSL --connect-timeout 60 -o input-sdk-download.zip "%WINSDK%"
 7z x input-sdk-download.zip -o%INPUT_SDK_DIR%
