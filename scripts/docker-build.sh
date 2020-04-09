6648e85a1e9cf31792c2d560b6218911935b1a55
Peter Petrik <zilolv@gmail.com>
2020-04-09 07:47:23 +0200

    fix android build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -48,14 +48,14 @@ if [ -f ${SOURCE_DIR}/Input_keystore.keystore ]; then
 	    --sign ${SOURCE_DIR}/Input_keystore.keystore input \
 	    --storepass ${STOREPASS} \
 	    --keypass ${STOREPASS} \
-        --input ${BUILD_DIR}/android-libInput.so-deployment-settings.json \
+        --input ${BUILD_DIR}/android-Input-deployment-settings.json \
 	    --output ${INSTALL_DIR} \
 	    --deployment bundled \
 	    --android-platform ${ANDROID_NDK_PLATFORM} \
 	    --gradle
 else
     ${QT_ANDROID}/bin/androiddeployqt \
-        --input ${BUILD_DIR}/android-libInput.so-deployment-settings.json \
+        --input ${BUILD_DIR}/android-Input-deployment-settings.json \
 	    --output ${INSTALL_DIR} \
 	    --deployment bundled \
 	    --android-platform ${ANDROID_NDK_PLATFORM} \
