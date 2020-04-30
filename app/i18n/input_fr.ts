f59935d2204a595d8bf869e795c5e23975a2ae99
Peter Petrik <zilolv@gmail.com>
2020-04-30 16:11:33 +0200

    add translations

---
+++ app/i18n/input_fr.ts
@@ -0,0 +1,307 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>AccountPage</name>
+    <message>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Used Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sign out</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ActiveLayerPanel</name>
+    <message>
+        <source>Select Active Layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No editable layers in the project!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AuthPanel</name>
+    <message>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sign in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mergin server has been updated. Please, update Input app to enable Mergin functionality.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mergin server unavailable.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sign up</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExternalResourceBundle</name>
+    <message>
+        <source>Open Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Image files (*.gif *.png *.jpg)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete photo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Would you like to permanently delete the image file?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FeaturePanel</name>
+    <message>
+        <source>Edit Object</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Object</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete feature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Really delete this feature?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FeatureToolbar</name>
+    <message>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit geometry</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPanel</name>
+    <message>
+        <source>Projects</source>
+        <translation>COOOL</translation>
+    </message>
+    <message>
+        <source>GPS</source>
+        <translation>GEPES</translation>
+    </message>
+    <message>
+        <source>Record</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom to project</source>
+        <translation>SHOW ME!</translation>
+    </message>
+    <message>
+        <source>Map themes</source>
+        <translation>Magic...</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>More</source>
+        <translation>Whatabout...</translation>
+    </message>
+</context>
+<context>
+    <name>MapThemePanel</name>
+    <message>
+        <source>Map Themes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No themes in the project!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MerginProjectPanel</name>
+    <message>
+        <source>Projects</source>
+        <translation type="unfinished">XXX</translation>
+    </message>
+    <message>
+        <source>SEARCH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Explore public Mergin projects!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to get the list of projects.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No projects found!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Project status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No write access</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>My projects</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Shared with me</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Shared</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Explore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you really want to delete project?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Try again</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProjectStatusPanel</name>
+    <message>
+        <source>No Changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>section</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RecordToolbar</name>
+    <message>
+        <source>GPS</source>
+        <translation type="unfinished">GEPES</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add Point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPanel</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>GPS</source>
+        <translation type="unfinished">GEPES</translation>
+    </message>
+    <message>
+        <source>Follow GPS with map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>GPS accuracy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Accuracy threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Line rec. interval</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No editable layers!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The GPS is currently not available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Synchronization error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
