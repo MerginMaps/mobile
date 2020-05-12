e993cfe42c4bad4ea46a23e2158ec9a32f458c80
Peter Petrik <zilolv@gmail.com>
2020-05-12 11:17:23 +0200

    maybe success?

--- scripts/upload-artifacts-ios.bash
+++ scripts/upload-artifacts-ios.bash
@@ -12,10 +12,10 @@ if [ "X${BUILD_FILE}" == "X" ]; then
     exit 1
 fi
 
-export IPA_FILE=input-ios-${GITHUB_SHA}-${ARCH}-${SIGNED}.apk
+export IPA_FILE=input-ios-${GITHUB_SHA}-${ARCH}-${SIGNED}.ipa
 export GITHUB_API=https://api.github.com/repos/${GITHUB_REPO}/commits/${GITHUB_SHA}/comments
 
-sudo cp ${BUILD_FILE} /tmp/${IPA_FILE}
-python3 ./scripts/uploader.py --source /tmp/${IPA_FILE} --destination "/$DROPBOX_FOLDER/${IPA_FILE}" --token INPUTAPP_BOT_DROPBOX_TOKEN > uploader.log 2>&1
+cp ${BUILD_FILE} ${IPA_FILE}
+python3 ./scripts/uploader.py --source ${IPA_FILE} --destination "/$DROPBOX_FOLDER/${IPA_FILE}" --token INPUTAPP_BOT_DROPBOX_TOKEN > uploader.log 2>&1
 APK_URL=`tail -n 1 uploader.log`
 curl -u inputapp-bot:${INPUTAPP_BOT_GITHUB_TOKEN} -X POST --data '{"body": "'${SIGNED}' ipa: ['${ARCH}']('${APK_URL}') (SDK: ['${SDK_VERSION}'](https://github.com/lutraconsulting/input-sdk/releases/tag/ios-'${SDK_VERSION}'))"}' ${GITHUB_API}
