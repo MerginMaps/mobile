8597dc9f1a977b0fb428706217f1729763b5bc12
tomasMizera <tomas.mizera2@gmail.com>
2020-06-26 10:45:16 +0200

    nit check all script

--- app/layerfeaturesmodel.cpp
+++ app/layerfeaturesmodel.cpp
@@ -1,43 +1,44 @@
 #include "layerfeaturesmodel.h"
 
-LayerFeaturesModel::LayerFeaturesModel(QObject *parent)
-  : QAbstractListModel(parent)
+LayerFeaturesModel::LayerFeaturesModel( QObject *parent )
+  : QAbstractListModel( parent )
 {
 }
 
-int LayerFeaturesModel::rowCount(const QModelIndex &parent) const
+int LayerFeaturesModel::rowCount( const QModelIndex &parent ) const
 {
   // For list models only the root node (an invalid parent) should return the list's size. For all
   // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
-  if (parent.isValid())
+  if ( parent.isValid() )
     return 0;
 
   // FIXME: Implement me!
   return 0;
 }
 
-QVariant LayerFeaturesModel::data(const QModelIndex &index, int role) const
+QVariant LayerFeaturesModel::data( const QModelIndex &index, int role ) const
 {
-  if (!index.isValid())
+  if ( !index.isValid() )
     return QVariant();
 
   // FIXME: Implement me!
   return QVariant();
 }
 
-bool LayerFeaturesModel::setData(const QModelIndex &index, const QVariant &value, int role)
+bool LayerFeaturesModel::setData( const QModelIndex &index, const QVariant &value, int role )
 {
-  if (data(index, role) != value) {
+  if ( data( index, role ) != value )
+  {
     // FIXME: Implement me!
-    emit dataChanged(index, index, QVector<int>() << role);
+    emit dataChanged( index, index, QVector<int>() << role );
     return true;
   }
   return false;
 }
 
-Qt::ItemFlags LayerFeaturesModel::flags(const QModelIndex &index) const
+Qt::ItemFlags LayerFeaturesModel::flags( const QModelIndex &index ) const
 {
-  if (!index.isValid())
+  if ( !index.isValid() )
     return Qt::NoItemFlags;
 
   return Qt::ItemIsEditable; // FIXME: Implement me!
--- app/layerfeaturesmodel.h
+++ app/layerfeaturesmodel.h
@@ -5,23 +5,23 @@
 
 class LayerFeaturesModel : public QAbstractListModel
 {
-  Q_OBJECT
+    Q_OBJECT
 
-public:
-  explicit LayerFeaturesModel(QObject *parent = nullptr);
+  public:
+    explicit LayerFeaturesModel( QObject *parent = nullptr );
 
-  // Basic functionality:
-  int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    // Basic functionality:
+    int rowCount( const QModelIndex &parent = QModelIndex() ) const override;
 
-  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant data( const QModelIndex &index, int role = Qt::DisplayRole ) const override;
 
-  // Editable:
-  bool setData(const QModelIndex &index, const QVariant &value,
-               int role = Qt::EditRole) override;
+    // Editable:
+    bool setData( const QModelIndex &index, const QVariant &value,
+                  int role = Qt::EditRole ) override;
 
-  Qt::ItemFlags flags(const QModelIndex& index) const override;
+    Qt::ItemFlags flags( const QModelIndex &index ) const override;
 
-private:
+  private:
 };
 
 #endif // LAYERFEATURESMODEL_H
