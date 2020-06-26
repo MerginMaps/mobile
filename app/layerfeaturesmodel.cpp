a5c6ebef047bf846a67e4e79ee406793c07105cb
tomasMizera <tomas.mizera2@gmail.com>
2020-06-26 10:43:49 +0200

    change model to qabstractlistmodel

--- app/layerfeaturesmodel.cpp
+++ app/layerfeaturesmodel.cpp
@@ -1,41 +1,44 @@
 #include "layerfeaturesmodel.h"
 
-layerfeaturesmodel::layerfeaturesmodel(QObject *parent)
-  : QAbstractItemModel(parent)
+LayerFeaturesModel::LayerFeaturesModel(QObject *parent)
+  : QAbstractListModel(parent)
 {
 }
 
-QModelIndex layerfeaturesmodel::index(int row, int column, const QModelIndex &parent) const
+int LayerFeaturesModel::rowCount(const QModelIndex &parent) const
 {
-  // FIXME: Implement me!
-}
-
-QModelIndex layerfeaturesmodel::parent(const QModelIndex &index) const
-{
-  // FIXME: Implement me!
-}
-
-int layerfeaturesmodel::rowCount(const QModelIndex &parent) const
-{
-  if (!parent.isValid())
+  // For list models only the root node (an invalid parent) should return the list's size. For all
+  // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
+  if (parent.isValid())
     return 0;
 
   // FIXME: Implement me!
+  return 0;
 }
 
-int layerfeaturesmodel::columnCount(const QModelIndex &parent) const
-{
-  if (!parent.isValid())
-    return 0;
-
-  // FIXME: Implement me!
-}
-
-QVariant layerfeaturesmodel::data(const QModelIndex &index, int role) const
+QVariant LayerFeaturesModel::data(const QModelIndex &index, int role) const
 {
   if (!index.isValid())
     return QVariant();
 
   // FIXME: Implement me!
   return QVariant();
+}
+
+bool LayerFeaturesModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+  if (data(index, role) != value) {
+    // FIXME: Implement me!
+    emit dataChanged(index, index, QVector<int>() << role);
+    return true;
+  }
+  return false;
+}
+
+Qt::ItemFlags LayerFeaturesModel::flags(const QModelIndex &index) const
+{
+  if (!index.isValid())
+    return Qt::NoItemFlags;
+
+  return Qt::ItemIsEditable; // FIXME: Implement me!
 }
--- app/layerfeaturesmodel.h
+++ app/layerfeaturesmodel.h
@@ -1,24 +1,25 @@
 #ifndef LAYERFEATURESMODEL_H
 #define LAYERFEATURESMODEL_H
 
-#include <QAbstractItemModel>
+#include <QAbstractListModel>
 
-class layerfeaturesmodel : public QAbstractItemModel
+class LayerFeaturesModel : public QAbstractListModel
 {
   Q_OBJECT
 
 public:
-  explicit layerfeaturesmodel(QObject *parent = nullptr);
+  explicit LayerFeaturesModel(QObject *parent = nullptr);
 
   // Basic functionality:
-  QModelIndex index(int row, int column,
-                    const QModelIndex &parent = QModelIndex()) const override;
-  QModelIndex parent(const QModelIndex &index) const override;
-
   int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-  int columnCount(const QModelIndex &parent = QModelIndex()) const override;
 
   QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+  // Editable:
+  bool setData(const QModelIndex &index, const QVariant &value,
+               int role = Qt::EditRole) override;
+
+  Qt::ItemFlags flags(const QModelIndex& index) const override;
 
 private:
 };
