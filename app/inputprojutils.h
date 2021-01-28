63be16956b10de275b878d7af93c0dfe90ac772b
Peter Petrik <zilolv@gmail.com>
2021-01-28 10:35:55 +0100

    add better docs

--- app/inputprojutils.h
+++ app/inputprojutils.h
@@ -14,11 +14,19 @@
 #include <QtGlobal>
 
 /**
- * Sets the PROJ environment variables to the default Input proj folder
- * and also to currently loaded folder <namespace>/<project>/proj to
- * search for PROJ resources
+ * QGIS uses PROJ library for doing CRS transformations.
+ * We currently ship the basic PROJ resources QGIS needs to do most common projections by adding them to the assets in APKs (see https://github.com/lutraconsulting/input/tree/master/app/android/assets/qgis-data). There could be a custom PROJ datum shifts used by users as part of their mergin projects in the `<project>/proj/` folder.
  *
- * Also handles the exceptions from the QGIS PROJ transformations
+ * PROJ library is set up in QGIS initialization function. Before calling this function, we need to
+ *  1. Expand the PROJ resources to the <device>/INPUT/proj folder
+ *  2. Clean the <device>/INPUT/proj_custom folder and copy all custom PROJ resources from all projects present on the device to the folder <device>/INPUT/proj_custom folder.
+ *  3. Set the defaul PROJ context with both paths from 1 and 2 with usage of `proj_context_set_search_paths`
+ *  4. now call qgis init function -> this takes all paths from proj_context and adds some internal QGIS paths too. This context is then used for all QGIS projection transformations
+ *
+ * Since QGIS uses only one context and there is not easy way how to dynamically refresh it after initialization, we are currently unable to dynamically add/remove custom datum shift files. Therefore if user download a project with custom proj files, the InputApp has to be restarted. Also there could be bugs if 2 projects uses the custom proj files with the same name but different content (unlikely).
+ *
+ * The InputProjUtils also adds QgsCoordinateTransform::setCustom*Handlers that pops out the dialog to user mentioning missing custom grids and action to restart the app.
+ *
  */
 class InputProjUtils: public QObject
 {
