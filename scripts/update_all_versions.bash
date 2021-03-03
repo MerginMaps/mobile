8de337d72a6a3767620bc125e45c12c04f3b74c8
Martin Dobias <wonder.sk@gmail.com>
2021-03-03 08:13:23 +0100

    Fix sed error when running update_all_versions.bash (#1215)

--- scripts/update_all_versions.bash
+++ scripts/update_all_versions.bash
@@ -17,23 +17,23 @@ echo "using version $MAJOR.$MINOR.$BUILD"
 IOS_FILE=$DIR/../app/ios/Info.plist
 echo "patching $IOS_FILE"
 
-sed -i .orig -E "s|<string>[0-9]+\\.[0-9]+\\.[0-9]+<\\/string>|<string>$VERSION<\\/string>|g" $IOS_FILE
+sed -i.orig -E "s|<string>[0-9]+\\.[0-9]+\\.[0-9]+<\\/string>|<string>$VERSION<\\/string>|g" $IOS_FILE
 rm -f $IOS_FILE.orig
 
 # android: app/version.pri
 ANDROID_FILE=$DIR/../app/version.pri
 echo "patching $ANDROID_FILE"
-sed -i .orig -E "s|VERSION_MAJOR = [0-9]+|VERSION_MAJOR = $MAJOR|g" $ANDROID_FILE
-sed -i .orig -E "s|VERSION_MINOR = [0-9]+|VERSION_MINOR = $MINOR|g" $ANDROID_FILE
-sed -i .orig -E "s|VERSION_FIX   = [0-9]+|VERSION_FIX   = $BUILD|g" $ANDROID_FILE
+sed -i.orig -E "s|VERSION_MAJOR = [0-9]+|VERSION_MAJOR = $MAJOR|g" $ANDROID_FILE
+sed -i.orig -E "s|VERSION_MINOR = [0-9]+|VERSION_MINOR = $MINOR|g" $ANDROID_FILE
+sed -i.orig -E "s|VERSION_FIX   = [0-9]+|VERSION_FIX   = $BUILD|g" $ANDROID_FILE
 rm -f $ANDROID_FILE.orig
 
 # win: scripts/version.cmd
 WIN_FILE=$DIR/version.cmd
 echo "patching $WIN_FILE"
-sed -i .orig -E "s|VERSIONMAJOR=[0-9]+|VERSIONMAJOR=$MAJOR|g" $WIN_FILE
-sed -i .orig -E "s|VERSIONMINOR=[0-9]+|VERSIONMINOR=$MINOR|g" $WIN_FILE
-sed -i .orig -E "s|VERSIONBUILD=[0-9]+|VERSIONBUILD=$BUILD|g" $WIN_FILE
+sed -i.orig -E "s|VERSIONMAJOR=[0-9]+|VERSIONMAJOR=$MAJOR|g" $WIN_FILE
+sed -i.orig -E "s|VERSIONMINOR=[0-9]+|VERSIONMINOR=$MINOR|g" $WIN_FILE
+sed -i.orig -E "s|VERSIONBUILD=[0-9]+|VERSIONBUILD=$BUILD|g" $WIN_FILE
 rm -f $WIN_FILE.orig
 
 echo "patching done"
