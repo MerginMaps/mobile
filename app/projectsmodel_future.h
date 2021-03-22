9006bcf109d7ac8269b758f25b92d8ea72a250f9
tomasMizera <tomas.mizera2@gmail.com>
2021-03-22 16:13:46 +0100

    add pagination

--- app/projectsmodel_future.h
+++ app/projectsmodel_future.h
@@ -56,6 +56,8 @@ class ProjectsModel_future : public QAbstractListModel
     ProjectsModel_future( MerginApi *merginApi, ProjectModelTypes modelType, LocalProjectsManager &localProjectsManager, QObject *parent = nullptr );
     ~ProjectsModel_future() override {};
 
+    Q_PROPERTY( int serverProjectsCount READ serverProjectsCount WRITE setServerProjectsCount NOTIFY serverProjectsCountChanged );
+
     // Needed methods from QAbstractListModel
     Q_INVOKABLE QVariant data( const QModelIndex &index, int role ) const override;
     Q_INVOKABLE QModelIndex index( int row, int column = 0, const QModelIndex &parent = QModelIndex() ) const override;
@@ -63,7 +65,7 @@ class ProjectsModel_future : public QAbstractListModel
     int rowCount( const QModelIndex &parent = QModelIndex() ) const override;
 
     //! Called to list projects, either fetch more or get first
-    Q_INVOKABLE void listProjects();
+    Q_INVOKABLE void listProjects( int page = 1, const QString searchExpression = QString() );
 
     //! Called to list projects, either fetch more or get first
     Q_INVOKABLE void listProjectsByName();
@@ -75,26 +77,35 @@ class ProjectsModel_future : public QAbstractListModel
     Q_INVOKABLE void stopProjectSync( QString projectNamespace, QString projectName );
 
     //! Method merging local and remote projects based on the model type
-    void mergeProjects( const MerginProjectsList &merginProjects, Transactions pendingProjects );
+    void mergeProjects( const MerginProjectsList &merginProjects, Transactions pendingProjects, bool keepPrevious = false );
 
-  public slots:
+    int serverProjectsCount() const;
+
+public slots:
+    // MerginAPI - backend signals
     void onListProjectsFinished( const MerginProjectsList &merginProjects, Transactions pendingProjects, int projectsCount, int page, QString requestId );
     void onListProjectsByNameFinished( const MerginProjectsList &merginProjects, Transactions pendingProjects, QString requestId );
     void onProjectSyncFinished( const QString &projectDir, const QString &projectFullName, bool successfully = true );
     void onProjectSyncProgressChanged( const QString &projectFullName, qreal progress );
+    void onProjectDetachedFromMergin( const QString &projectFullName );
+    void onProjectAttachedToMergin( const QString &projectFullName );
 
+    // LocalProjectsManager signals
     void onProjectAdded( const LocalProject_future &project );
     void onAboutToRemoveProject( const LocalProject_future project );
     void onProjectDataChanged( const LocalProject_future &project );
 
-    void onProjectDetachedFromMergin( const QString &projectFullName );
-    void onProjectAttachedToMergin( const QString &projectFullName );
+    void setServerProjectsCount( int serverProjectsCount );
 
-  private:
+signals:
+    void serverProjectsCountChanged( int serverProjectsCount );
+
+private:
 
     QString modelTypeToFlag() const;
     void printProjects() const;
     QStringList projectNames() const;
+    void loadLocalProjects();
 
     bool containsProject( QString projectId ) const;
     std::shared_ptr<Project_future> projectFromId( QString projectId ) const;
@@ -106,7 +117,7 @@ class ProjectsModel_future : public QAbstractListModel
     ProjectModelTypes mModelType;
 
     //! For pagination
-    int mPopulatedPage = -1; // -> on the fly in QML:: QML should pass this to model
+    int mServerProjectsCount = -1;
 
     //! For processing only my requests
     QString mLastRequestId;
