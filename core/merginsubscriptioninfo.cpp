8a1bc2ede0e4c230a5a83316cd0c8d7ed048d90b
vsklencar <viktor.sklencar@lutraconsulting.co.uk>
2021-04-22 15:05:42 +0200

    Exctracted subscription related info - moved files to core

---
+++ core/merginsubscriptioninfo.cpp
@@ -0,0 +1,187 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "merginsubscriptioninfo.h"
+#include "coreutils.h"
+
+MerginSubscriptionInfo::MerginSubscriptionInfo( QObject *parent )
+  : QObject( parent )
+{
+  clear();
+}
+
+void MerginSubscriptionInfo::clearSubscriptionData()
+{
+  mSubscriptionStatus = MerginSubscriptionStatus::FreeSubscription;
+  mSubscriptionTimestamp = "";
+  mNextBillPrice = "";
+  mSubscriptionId = -1;
+  mOwnsActiveSubscription = false;
+}
+
+void MerginSubscriptionInfo::clearPlanInfo()
+{
+  mPlanAlias = "";
+  mPlanProvider = MerginSubscriptionType::NoneSubscriptionType;
+  mPlanProductId = "";
+  emit planProviderChanged();
+  emit planProductIdChanged();
+}
+
+void MerginSubscriptionInfo::clear()
+{
+  clearSubscriptionData();
+//  mDiskUsage = 0;
+//  mStorageLimit = 0;
+  clearPlanInfo();
+
+  emit subscriptionInfoChanged();
+}
+
+void MerginSubscriptionInfo::setFromJson( QJsonObject docObj )
+{
+//  QJsonObject profileObj = docObj.value( QStringLiteral( "profile" ) ).toObject();
+//  mStorageLimit = profileObj.value( QStringLiteral( "storage" ) ).toDouble();
+
+  // parse service data
+  QJsonObject serviceObj = docObj.value( QStringLiteral( "service" ) ).toObject();
+
+  // parse service.subscription data
+  QJsonObject subscriptionObj = serviceObj.value( QStringLiteral( "subscription" ) ).toObject();
+  if ( subscriptionObj.isEmpty() )
+  {
+    // only free plan is assigned, subscription data is not present in JSON
+    clearSubscriptionData();
+  }
+  else
+  {
+    // user has subscription
+
+    mNextBillPrice = subscriptionObj.value( QStringLiteral( "next_bill_price" ) ).toString();
+    QString nextPaymentDate = subscriptionObj.value( QStringLiteral( "next_payment" ) ).toString();
+    QString validUntil = subscriptionObj.value( QStringLiteral( "valid_until" ) ).toString();
+    if ( nextPaymentDate.isEmpty() )
+    {
+      mSubscriptionTimestamp = CoreUtils::localizedDateFromUTFString( validUntil );
+    }
+    else
+    {
+      mSubscriptionTimestamp = CoreUtils::localizedDateFromUTFString( nextPaymentDate );
+    }
+
+    mSubscriptionId = subscriptionObj.value( QStringLiteral( "id" ) ).toInt();
+    QString status = subscriptionObj.value( QStringLiteral( "status" ) ).toString();
+
+    if ( status == "active" )
+    {
+      if ( nextPaymentDate.isEmpty() )
+      {
+        mSubscriptionStatus = MerginSubscriptionStatus::SubscriptionUnsubscribed;
+      }
+      else
+      {
+        mSubscriptionStatus = MerginSubscriptionStatus::ValidSubscription;
+      }
+    }
+
+    else if ( status == "past_due" )
+    {
+      mSubscriptionStatus = MerginSubscriptionStatus::SubscriptionInGracePeriod;
+    }
+    else // cancelled
+    {
+      mSubscriptionStatus = MerginSubscriptionStatus::CanceledSubscription;
+    }
+  }
+
+  // parse service.plan data
+  QJsonObject planObj = serviceObj.value( QStringLiteral( "plan" ) ).toObject();
+  mOwnsActiveSubscription = planObj.value( QStringLiteral( "is_paid_plan" ) ).toBool();
+  mPlanAlias = planObj.value( QStringLiteral( "alias" ) ).toString();
+
+
+  MerginSubscriptionType::SubscriptionType planProvider = MerginSubscriptionType::fromString( planObj.value( QStringLiteral( "type" ) ).toString() );
+  if ( planProvider != mPlanProvider )
+  {
+    mPlanProvider = planProvider;
+    emit planProviderChanged();
+  }
+  QString planProductId = planObj.value( QStringLiteral( "product_id" ) ).toString();
+  if ( planProductId !=  mPlanProductId )
+  {
+    mPlanProductId = planProductId;
+    emit planProductIdChanged();
+  }
+
+  emit subscriptionInfoChanged();
+}
+
+bool MerginSubscriptionInfo::ownsActiveSubscription() const
+{
+  return mOwnsActiveSubscription;
+}
+
+void MerginSubscriptionInfo::setLocalizedPrice( const QString &price )
+{
+  if ( price.isEmpty() )
+    return;
+
+  if ( price != mNextBillPrice )
+  {
+    mNextBillPrice = price;
+    emit subscriptionInfoChanged();
+  }
+}
+
+
+int MerginSubscriptionInfo::subscriptionId() const
+{
+  return mSubscriptionId;
+}
+
+
+MerginSubscriptionType::SubscriptionType MerginSubscriptionInfo::planProvider() const
+{
+  return mPlanProvider;
+}
+
+QString MerginSubscriptionInfo::planProductId() const
+{
+  return mPlanProductId;
+}
+
+QString MerginSubscriptionInfo::planAlias() const
+{
+  return mPlanAlias;
+}
+
+QString MerginSubscriptionInfo::nextBillPrice() const
+{
+  return mNextBillPrice;
+}
+
+int MerginSubscriptionInfo::subscriptionStatus() const
+{
+  return mSubscriptionStatus;
+}
+
+//double MerginSubscriptionInfo::diskUsage() const
+//{
+//  return mDiskUsage;
+//}
+
+//double MerginSubscriptionInfo::storageLimit() const
+//{
+//  return mStorageLimit;
+//}
+
+QString MerginSubscriptionInfo::subscriptionTimestamp() const
+{
+  return mSubscriptionTimestamp;
+}
---
+++ core/merginsubscriptioninfo.h
@@ -0,0 +1,72 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef MERGINSUBSCRIPTIONINFO_H
+#define MERGINSUBSCRIPTIONINFO_H
+
+
+#include <QObject>
+#include <QString>
+#include <QJsonObject>
+
+#include "merginsubscriptionstatus.h"
+#include "merginsubscriptiontype.h"
+
+class MerginSubscriptionInfo: public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY( QString planAlias READ planAlias NOTIFY subscriptionInfoChanged ) // see PurchasingPlan::alias()
+    Q_PROPERTY( /*MerginSubscriptionType::SubscriptionType*/ int planProvider READ planProvider NOTIFY subscriptionInfoChanged ) // see PurchasingTransaction::provider()
+    Q_PROPERTY( QString planProductId READ planProductId NOTIFY subscriptionInfoChanged ) // see PurchasingPlan::id()
+    Q_PROPERTY( /*MerginSubscriptionStatus::SubscriptionStatus*/ int subscriptionStatus READ subscriptionStatus NOTIFY subscriptionInfoChanged )
+    Q_PROPERTY( QString subscriptionTimestamp READ subscriptionTimestamp NOTIFY subscriptionInfoChanged )
+    Q_PROPERTY( QString nextBillPrice READ nextBillPrice NOTIFY subscriptionInfoChanged ) // in Bytes
+    Q_PROPERTY( bool ownsActiveSubscription READ ownsActiveSubscription NOTIFY subscriptionInfoChanged )
+
+  public:
+    explicit MerginSubscriptionInfo( QObject *parent = nullptr );
+    ~MerginSubscriptionInfo() = default;
+
+  public:
+    void clearSubscriptionData();
+    void clearPlanInfo();
+    void clear();
+
+    QString planAlias() const;
+    int subscriptionId() const;
+    MerginSubscriptionType::SubscriptionType planProvider() const;
+    QString planProductId() const;
+    QString nextBillPrice() const;
+    /*MerginSubscriptionStatus::SubscriptionStatus*/ int subscriptionStatus() const;
+    QString subscriptionTimestamp() const;
+    bool ownsActiveSubscription() const;
+
+    void setLocalizedPrice( const QString &price );
+    void setFromJson( QJsonObject docObj );
+    void setSubscriptionInfoFromJson( QJsonObject docObj );
+
+  signals:
+    void subscriptionInfoChanged();
+    void planProductIdChanged();
+    void planProviderChanged();
+    void storageChanged( double storage );
+
+  private:
+    QString mPlanAlias;
+    int mSubscriptionId = -1;
+    MerginSubscriptionType::SubscriptionType mPlanProvider = MerginSubscriptionType::NoneSubscriptionType;
+    QString mPlanProductId;
+    bool mOwnsActiveSubscription = false;
+    QString mNextBillPrice;
+    MerginSubscriptionStatus::SubscriptionStatus mSubscriptionStatus = MerginSubscriptionStatus::FreeSubscription;
+    QString mSubscriptionTimestamp;
+};
+
+#endif // MERGINSUBSCRIPTIONINFO_H
