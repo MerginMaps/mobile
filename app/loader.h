66106ce69d271c4c3aef0c45a4a55cce6683dc8f
Tomas Mizera <tomas.mizera@lutraconsulting.co.uk>
2021-06-15 12:08:20 +0200

    Replaced name property with instance itself for map layer (#1471)
    rename setActiveLayer function to avoid overload

--- app/loader.h
+++ app/loader.h
@@ -65,7 +65,7 @@ class Loader: public QObject
     /**
      * setActiveLayer sets active layer from layer name
      */
-    Q_INVOKABLE void setActiveLayer( QString layerName ) const;
+    void setActiveLayerByName( QString layerName ) const;
 
     /**
      * setActiveLayer sets active layer from layer
