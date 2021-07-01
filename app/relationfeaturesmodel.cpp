6980b87462cef6d20d7509bf4db6c436e561d256
sklencar <vsklencar@gmail.com>
2021-07-01 11:47:29 +0200

    Added gallery widget for relations (#1527)
    * Added gallery widget for relations
    
    * Some improvements of image gallery for relations
    
    * Preview image path resolved by inputUtils fn
    
    Co-authored-by: vsklencar <viktor.sklencar@lutraconsulting.co.uk>

--- app/relationfeaturesmodel.cpp
+++ app/relationfeaturesmodel.cpp
@@ -8,10 +8,37 @@
  ***************************************************************************/
 
 #include "relationfeaturesmodel.h"
+#include "inpututils.h"
 
 RelationFeaturesModel::RelationFeaturesModel( QObject *parent )
   : FeaturesListModel( parent )
 {
+}
+
+QVariant RelationFeaturesModel::data( const QModelIndex &index, int role ) const
+{
+  int row = index.row();
+  if ( row < 0 || row >= mFeatures.count() )
+    return QVariant();
+
+  if ( !index.isValid() )
+    return QVariant();
+
+  if ( role == PhotoPath )
+  {
+    const FeatureLayerPair pair = mFeatures.at( index.row() );
+    return relationPhotoPath( pair );
+  }
+  else
+    return FeaturesListModel::data( index, role );
+}
+
+QHash<int, QByteArray> RelationFeaturesModel::roleNames() const
+{
+  QHash<int, QByteArray> roles = FeaturesListModel::roleNames();
+  roles[PhotoPath] = QStringLiteral( "PhotoPath" ).toLatin1();
+
+  return roles;
 }
 
 void RelationFeaturesModel::setup()
@@ -36,6 +63,8 @@ void RelationFeaturesModel::populate()
   }
 
   endResetModel();
+
+  setIsTextType( photoFieldIndex( mRelation.referencingLayer() ) == -1 );
 }
 
 void RelationFeaturesModel::setParentFeatureLayerPair( FeatureLayerPair pair )
@@ -68,4 +97,63 @@ FeatureLayerPair RelationFeaturesModel::parentFeatureLayerPair() const
 QgsRelation RelationFeaturesModel::relation() const
 {
   return mRelation;
+}
+
+QVariant RelationFeaturesModel::relationPhotoPath( const FeatureLayerPair &featurePair ) const
+{
+  // Feature title used to get path of the referenced image.
+  QString path = featureTitle( featurePair ).toString();
+
+  int fieldIndex = photoFieldIndex( featurePair.layer() );
+  QgsEditorWidgetSetup setup = featurePair.layer()->editorWidgetSetup( fieldIndex );
+  QVariantMap config = setup.config();
+
+  QString finalPath = InputUtils::resolvePath( path, homePath(), config, featurePair, QgsProject::instance() );
+
+  return QVariant( finalPath );
+}
+
+int RelationFeaturesModel::photoFieldIndex( QgsVectorLayer *layer ) const
+{
+
+  QgsFields fields = layer->fields();
+  for ( int i = 0; i < fields.size(); i++ )
+  {
+    // Lets try by widget type
+    QgsEditorWidgetSetup setup = layer->editorWidgetSetup( i );
+    QVariantMap config = setup.config();
+    if ( setup.type() == QStringLiteral( "ExternalResource" ) )
+    {
+      return i;
+    }
+  }
+  return -1;
+}
+
+bool RelationFeaturesModel::isTextType() const
+{
+  return mIsTextType;
+}
+
+void RelationFeaturesModel::setIsTextType( bool isTextType )
+{
+  if ( isTextType != mIsTextType )
+  {
+    mIsTextType = isTextType;
+    emit isTextTypeChanged();
+  }
+}
+
+QString RelationFeaturesModel::homePath() const
+{
+  return mHomePath;
+}
+
+void RelationFeaturesModel::setHomePath( const QString &homePath )
+{
+  if ( homePath != mHomePath )
+  {
+    mHomePath = homePath;
+    emit homePathChanged();
+  }
 }
--- app/relationfeaturesmodel.h
+++ app/relationfeaturesmodel.h
@@ -23,13 +23,34 @@ class RelationFeaturesModel : public FeaturesListModel
 
     Q_PROPERTY( QgsRelation relation READ relation WRITE setRelation NOTIFY relationChanged )
 
+    //! Home path of the active project used to evaluate absolute path of referencved images
+    Q_PROPERTY( QString homePath READ homePath WRITE setHomePath NOTIFY homePathChanged )
+
     //! parent feature layer pair represents a feature from parent relation layer for which we gather related child features
     Q_PROPERTY( FeatureLayerPair parentFeatureLayerPair READ parentFeatureLayerPair WRITE setParentFeatureLayerPair NOTIFY parentFeatureLayerPairChanged )
 
+    /**
+     * Flag to distinquash what data type suppose to be displayed. By default text data type is expected, otherwise image.
+     * Property is set to False only if there is any photo field (see more: RelationFeaturesModel::photoFieldIndex)
+     */
+    Q_PROPERTY( bool isTextType READ isTextType WRITE setIsTextType NOTIFY isTextTypeChanged )
+
   public:
+
+    //! Roles for RelationFeaturesListModel
+    enum relationModelRoles
+    {
+      PhotoPath = Qt::UserRole + 100,
+    };
+    Q_ENUM( relationModelRoles );
+
 
     explicit RelationFeaturesModel( QObject *parent = nullptr );
     virtual ~RelationFeaturesModel() {};
+
+    // QAbstractItemModel interface
+    QVariant data( const QModelIndex &index, int role ) const override;
+    QHash<int, QByteArray> roleNames() const override;
 
     void setup(); // will be override
     void populate(); // will be override
@@ -40,14 +61,32 @@ class RelationFeaturesModel : public FeaturesListModel
     FeatureLayerPair parentFeatureLayerPair() const;
     QgsRelation relation() const;
 
+    QString homePath() const;
+    void setHomePath( const QString &homePath );
+
+    bool isTextType() const;
+    void setIsTextType( bool isTextType );
+
   signals:
     void parentFeatureLayerPairChanged( FeatureLayerPair pair );
     void relationChanged( QgsRelation relation );
+    void homePathChanged();
+    void isTextTypeChanged();
 
   private:
+    QVariant relationPhotoPath( const FeatureLayerPair &featurePair ) const;
+
+    /**
+     * Searches and returns first index of photo field if the field is type of 'ExternalResource'.
+     * @param layer Referencing layer of the relation.
+     * @return Index of photo field, otherwise -1 if not found any.
+     */
+    int photoFieldIndex( QgsVectorLayer *layer ) const;
 
     QgsRelation mRelation; // associated relation
     FeatureLayerPair mParentFeatureLayerPair; // parent feature (with relation widget in form)
+    QString mHomePath;
+    bool mIsTextType = true;
 };
 
 #endif // RELATIONFEATURESMODEL_H
