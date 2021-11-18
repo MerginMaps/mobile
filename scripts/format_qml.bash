9d263c560e377c7ccc852a4d92ac0c8b16b04ffe
tomasMizera <tomas.mizera@lutraconsulting.co.uk>
2021-11-18 15:56:36 +0100

    actions v5

--- scripts/format_qml.bash
+++ scripts/format_qml.bash
@@ -1,39 +1,55 @@
 #!/bin/bash
 
-# iterate over qml files and run qmlformat
-
 echo "QML STYLE CHECK STARTED"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PWD=`pwd`
 cd $DIR
 
-FORMATTER=""
+# We are currently using qmlfmt program available from homebrew on mac.
+# It is available here: https://github.com/jesperhh/qmlfmt
+# Ideally we should be using qmlformat tool from Qt, however due to a lack
+# of their functionality (and bugs) we moved to qmlfmt.
+# See https://bugreports.qt.io/browse/QTBUG-98422 - if this bug is fixed and
+# released, we can move back to Qt's qmlformat.
 
-if [ $# -eq 0 ]; then
-    # No arguments provided, need to look for qmlformat in path
-    env
-    which qmlformat
+# Code that is commented out is a leftover from using qmlformat, it is kept
+# to be used in future
 
-    if [ $? -ne 0 ]; then
-    	echo "[!] qmlformat not found in PATH" >&2
-    	exit 1
-    fi
+# <QMLFORMAT CODE>
+# FORMATTER=""
+#
+# if [ $# -eq 0 ]; then
+#     # No arguments provided, need to look for qmlformat in path
+#     which qmlformat
+#
+#     if [ $? -ne 0 ]; then
+#     	echo "[!] qmlformat not found in PATH" >&2
+#     	exit 1
+#     fi
+#
+#     FORMATTER=`which qmlformat`
+# else
+#     FORMATTER=`which $1`
+#
+#     if [ $? -ne 0 ]; then
+#       echo "[!] passed qmlformat is invalid" >&2
+#       exit 1
+#     fi
+# fi
+#
+# ${FORMATTER} -v | grep -q "6."
+#
+# if [ $? -ne 0 ]; then
+#   echo "[!] qmlformat needs to be from Qt version 6.2 or higher! "
+#   exit 1
+# fi
+# </QMLFORMAT CODE>
 
-    FORMATTER=`which qmlformat`
-else
-    FORMATTER=`which $1`
-
-    if [ $? -ne 0 ]; then
-      echo "[!] passed qmlformat is invalid" >&2
-      exit 1
-    fi
-fi
-
-${FORMATTER} -v | grep -q "6."
+FORMATTER=`which qmlfmt`
 
 if [ $? -ne 0 ]; then
-  echo "[!] qmlformat needs to be from Qt version 6.2 or higher! "
+  echo "[!] qmlfmt program not found! Path: ${PATH}"
   exit 1
 fi
 
@@ -41,11 +57,15 @@ echo "Starting to format QML files"
 
 RETURN=0
 
-FILES=`find ../app ../qgsquick -name \*.qml* -print`
+FILES=`find ../app ../qgsquick -name \*.qml* -print | grep "editor" | grep "text"`
 
+# <QMLFORMAT CODE>
 # --indent-width 2: use 2 spaces to indent
 # --normalize: reorder properties, includes and childs in components
-OPTIONS="--indent-width 2 --normalize"
+#OPTIONS="--indent-width 2 --normalize"
+# </QMLFORMAT CODE>
+
+OPTIONS="-i 2"
 
 for FILE in $FILES; do
     if [[ $FILE =~ \.qml$ ]]; then
