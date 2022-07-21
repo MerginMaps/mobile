d5b291b865b2ad7a81865fc5e93c71407a297d1d
Tomas Mizera <tomas.mizera@lutraconsulting.co.uk>
2022-07-21 09:28:18 +0200

    [GEOEDIT] Alter guideline - move vertex with crosshair (#2172)
    * move vertex with cursor in guideline
    
    * add forgotten class

--- app/guidelinecontroller.h
+++ app/guidelinecontroller.h
@@ -16,6 +16,8 @@
 #include "qgsgeometry.h"
 #include "qgsquickmapsettings.h"
 
+#include "qgsvertexid.h"
+
 class GuidelineController : public QObject
 {
     Q_OBJECT
@@ -24,6 +26,8 @@ class GuidelineController : public QObject
     Q_PROPERTY( QgsGeometry realGeometry READ realGeometry WRITE setRealGeometry NOTIFY realGeometryChanged )
     Q_PROPERTY( QPointF crosshairPosition READ crosshairPosition WRITE setCrosshairPosition NOTIFY crosshairPositionChanged )
     Q_PROPERTY( QgsQuickMapSettings *mapSettings READ mapSettings WRITE setMapSettings NOTIFY mapSettingsChanged )
+
+    Q_PROPERTY( QgsVertexId activeVertexId READ activeVertexId WRITE setActiveVertexId NOTIFY activeVertexIdChanged )
 
     // output properties (real geometry + crosshair position ) in map CRS
     Q_PROPERTY( QgsGeometry guidelineGeometry READ guidelineGeometry WRITE setGuidelineGeometry NOTIFY guidelineGeometryChanged )
@@ -43,6 +47,9 @@ class GuidelineController : public QObject
     QgsQuickMapSettings *mapSettings() const;
     void setMapSettings( QgsQuickMapSettings *newMapSettings );
 
+    const QgsVertexId &activeVertexId() const;
+    void setActiveVertexId( const QgsVertexId &newActiveVertexId );
+
   signals:
 
     void guidelineGeometryChanged( const QgsGeometry &guidelineGeometry );
@@ -52,6 +59,8 @@ class GuidelineController : public QObject
 
     void mapSettingsChanged( QgsQuickMapSettings *mapSettings );
 
+    void activeVertexIdChanged( const QgsVertexId &activeVertexId );
+
   private:
     void buildGuideline();
 
@@ -59,6 +68,7 @@ class GuidelineController : public QObject
     QPointF mCrosshairPosition;
     QgsGeometry mRealGeometry;
     QgsQuickMapSettings *mMapSettings = nullptr; // not owned
+    QgsVertexId mActiveVertexId;
 };
 
 #endif // GUIDELINECONTROLLER_H
