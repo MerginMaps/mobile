1f30ea6078f999b6b754b99b2aa891b2dfd49d0e
Tomas Mizera <tomas.mizera@lutraconsulting.co.uk>
2022-07-29 10:10:12 +0200

    close app gracefully via Java for Huawei devices

--- app/android/src/uk/co/lutraconsulting/InputActivity.java
+++ app/android/src/uk/co/lutraconsulting/InputActivity.java
@@ -2,9 +2,14 @@ package uk.co.lutraconsulting;
 import android.os.Bundle;
 import android.view.WindowManager;
 import org.qtproject.qt5.android.bindings.QtActivity;
+import android.util.Log;
+import android.os.Build;
+import java.lang.Exception;
 
 public class InputActivity extends QtActivity
 {
+  private static final String TAG = "Mergin Maps Input Activity";
+
   @Override
   public void onCreate(Bundle savedInstanceState)
   {
@@ -14,6 +19,41 @@ public class InputActivity extends QtActivity
     // go into sleep and recording is not interrupted
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
   }
+
+  public void quitGracefully()
+  {
+    String man = android.os.Build.MANUFACTURER.toUpperCase();
+
+    Log.d( TAG, String.format("quitGracefully() from Java, MANUFACTURER: '%s'", man ) );
+    
+    //
+    // QT app exit on back button causes crashes on some manufacturers (mainly Huawei, but also Samsung Galaxy recently).
+    //
+    // Let's play safe and only use this fix for HUAWEI phones for now.
+    // If the fix proves itself in next release, we can add it for all other manufacturers.
+    //
+    // Qt bug: QTBUG-82617
+    // See: https://developernote.com/2022/03/crash-at-std-thread-and-std-mutex-destructors-on-android/#comment-694101
+    // See: https://stackoverflow.com/questions/61321845/qt-app-crashes-at-the-destructor-of-stdthread-on-android-10-devices
+    //
+
+    boolean shouldQuit = man.contains( "HUAWEI" );
+
+    if ( shouldQuit )
+    {
+      try
+      {
+        finishAffinity();
+        System.exit(0);
+      }
+      catch ( Exception exp )
+      {
+        exp.printStackTrace();
+        Log.d( TAG, String.format( "quitGracefully() failed to execute: '%s'", exp.toString() ) );
+      }
+    }
+  }
+
   @Override
   protected void onDestroy()
   {
