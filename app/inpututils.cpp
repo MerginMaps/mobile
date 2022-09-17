b4aab3f50783a1c8df73d3de698c8ab57ea33846
Alexander Bruy <alexander.bruy@gmail.com>
2022-09-17 14:25:54 +0300

    fix

--- app/inpututils.cpp
+++ app/inpututils.cpp
@@ -1072,8 +1072,8 @@ const QgsEditorWidgetSetup InputUtils::getEditorWidgetSetup( const QgsField &fie
   }
   else
   {
-    QVariantMap config;
-    config = config.unite( additionalArgs );
+    QMultiMap<QString, QVariant> config;
+    config = config.unite( QMultiMap( additionalArgs ) );
 
     if ( widgetType == QStringLiteral( "TextEdit" ) )
     {
@@ -1106,7 +1106,14 @@ const QgsEditorWidgetSetup InputUtils::getEditorWidgetSetup( const QgsField &fie
       config.insert( QStringLiteral( "AllowNULL" ), true );
     }
 
-    return QgsEditorWidgetSetup( widgetType, config );
+    QVariantMap cfg;
+    QList<QString> keys = config.uniqueKeys();
+    for ( int i = 0; i < keys.size(); i++ )
+    {
+      cfg.insert( keys.at( i ), config.value( keys.at( i ) ) );
+    }
+
+    return QgsEditorWidgetSetup( widgetType, cfg );
   }
 }
 
