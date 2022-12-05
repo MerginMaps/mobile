a53b18e76fc76225c8c81f4b64e9e16b28fad01b
Peter Petrik <zilolv@gmail.com>
2022-12-05 13:39:48 +0100

    Fix macos package (#2442)
    Fix macos release build

--- cmake/InstallStep.cmake
+++ cmake/InstallStep.cmake
@@ -1,17 +1,67 @@
 # GPLv2 Licence
 
+set(Qt6_base_dir ${Qt6_DIR}/../../..)
+
+# ########################################################################################
+# Binary
+# ########################################################################################
+
 if (WIN)
-  set(Qt6_base_dir ${Qt6_DIR}/../../..)
-  # Binary
   install(
     TARGETS Input
     LIBRARY DESTINATION lib/
     RUNTIME DESTINATION .
+  )
+else ()
+  install(
+    TARGETS Input
+    LIBRARY DESTINATION lib/
     BUNDLE DESTINATION .
   )
+endif ()
 
-  set(regex_no_db_libs "[(A-Z)|(a-z)|(0-9)|_][^d]\.dll")
-  # Libraries
+# ########################################################################################
+# Use <plat>deployqt when possible
+# ########################################################################################
+if (MACOS)
+  get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+  if (isMultiConfig)
+    set(deploy_script "${CMAKE_CURRENT_BINARY_DIR}/deploy_input_$<CONFIG>.cmake")
+  else ()
+    set(deploy_script
+        "${CMAKE_CURRENT_BINARY_DIR}/deploy_input_${CMAKE_BUILD_TYPE}.cmake"
+    )
+  endif ()
+
+  set(executable_path "Input.app/Contents/MacOS/INPUT")
+
+  # https://doc-snapshots.qt.io/qt6-dev/qt-deploy-runtime-dependencies.html Replace with
+  # qt_generate_deploy_script from QT 6.5.x The following script must only be executed at
+  # install time Note: This command is in technology preview and may change in future
+  # releases. (QT 6.4.x)
+  file(
+    GENERATE
+    OUTPUT ${deploy_script}
+    CONTENT
+      "
+		include(\"${QT_DEPLOY_SUPPORT}\")
+
+        qt_deploy_qml_imports(TARGET Input)
+
+    	qt_deploy_runtime_dependencies(
+            QML_DIR \"${qml_path}\"
+			EXECUTABLE \"${executable_path}\"
+			GENERATE_QT_CONF
+		)"
+  )
+
+  install(SCRIPT ${deploy_script})
+endif ()
+
+# ########################################################################################
+# SDK Shared Libraries
+# ########################################################################################
+if (WIN)
   install(
     DIRECTORY ${INPUT_SDK_PATH_MULTI}/bin/
     DESTINATION .
@@ -31,172 +81,188 @@ if (WIN)
     FILES_MATCHING
     PATTERN "*.dll"
   )
-
-  # Qt
-  set(qml_dirs
-      Qt
-      Qt5Compat
-      QtMultimedia
-      QtQml
-      QtQuick
-      QtPositioning
-      QtCore
-      QtSensors
+elseif (LNX)
+  install(
+    DIRECTORY ${INPUT_SDK_PATH_MULTI}/lib/
+    DESTINATION lib/
+    FILES_MATCHING
+    PATTERN "*.so"
+    PATTERN "qca-qt6" EXCLUDE
   )
-  foreach (qml_dir ${qml_dirs})
+  install(
+    DIRECTORY ${INPUT_SDK_PATH_MULTI}/lib/qca-qt6/crypto/
+    DESTINATION lib/
+    FILES_MATCHING
+    PATTERN "*.so"
+  )
+endif ()
+
+# ########################################################################################
+# QT Libraries
+# ########################################################################################
+set(qml_dirs
+    Qt
+    Qt5Compat
+    QtMultimedia
+    QtQml
+    QtQuick
+    QtPositioning
+    QtCore
+    QtSensors
+)
+
+foreach (qml_dir ${qml_dirs})
+  if (WIN)
     install(
       DIRECTORY ${Qt6_base_dir}/qml/${qml_dir}
       DESTINATION qml
       PATTERN "*d.dll" EXCLUDE
     )
-  endforeach ()
+  elseif (LNX)
+    install(
+      DIRECTORY ${Qt6_base_dir}/qml/${qml_dir}
+      DESTINATION qml
+      PATTERN "*d.so" EXCLUDE
+    )
+  endif ()
 
-  set(plugins_dirs
-      position
-      sqldrivers
-      imageformats
-      platforms
-      multimedia
-  )
-  foreach (plugins_dir ${plugins_dirs})
+endforeach ()
+
+set(plugins_dirs
+    position
+    sqldrivers
+    imageformats
+    platforms
+    multimedia
+)
+
+foreach (plugins_dir ${plugins_dirs})
+
+  if (WIN)
     install(
       DIRECTORY ${Qt6_base_dir}/plugins/${plugins_dir}
       DESTINATION .
       PATTERN "*d.dll" EXCLUDE
     )
-  endforeach ()
+  elseif (LNX)
+    install(
+      DIRECTORY ${Qt6_base_dir}/plugins/${plugins_dir}
+      DESTINATION plugins/
+      PATTERN "*d.so" EXCLUDE
+    )
+  endif ()
 
-  set(qt_libs
-      Qt6Bluetooth
-      Qt6Concurrent
-      Qt6Core
-      Qt6Core5Compat
-      Qt6DBus
-      Qt6Designer
-      Qt6DesignerComponents
-      Qt6Gui
-      Qt6Help
-      Qt6LabsAnimation
-      Qt6LabsFolderListModel
-      Qt6LabsQmlModels
-      Qt6LabsSettings
-      Qt6LabsSharedImage
-      Qt6LabsWavefrontMesh
-      Qt6Multimedia
-      Qt6MultimediaQuick
-      Qt6MultimediaWidgets
-      Qt6Network
-      Qt6Nfc
-      Qt6OpenGL
-      Qt6OpenGLWidgets
-      Qt6Positioning
-      Qt6PositioningQuick
-      Qt6PrintSupport
-      Qt6Qml
-      Qt6QmlCompiler
-      Qt6QmlCore
-      Qt6QmlLocalStorage
-      Qt6QmlModels
-      Qt6QmlWorkerScript
-      Qt6QmlXmlListModel
-      Qt6Quick
-      Qt6Quick3DSpatialAudio
-      Qt6QuickControls2
-      Qt6QuickControls2Impl
-      Qt6QuickDialogs2
-      Qt6QuickDialogs2QuickImpl
-      Qt6QuickDialogs2Utils
-      Qt6QuickLayouts
-      Qt6QuickParticles
-      Qt6QuickShapes
-      Qt6QuickTemplates2
-      Qt6QuickTest
-      Qt6QuickWidgets
-      Qt6Sensors
-      Qt6SensorsQuick
-      Qt6ShaderTools
-      Qt6SpatialAudio
-      Qt6Sql
-      Qt6Svg
-      Qt6SvgWidgets
-      Qt6Test
-      Qt6UiTools
-      Qt6Widgets
-      Qt6Xml
-      d3dcompiler_47
-      opengl32sw
+endforeach ()
+
+set(qt_libs
+    Bluetooth
+    Concurrent
+    Core
+    Core5Compat
+    DBus
+    Designer
+    DesignerComponents
+    Gui
+    Help
+    LabsAnimation
+    LabsFolderListModel
+    LabsQmlModels
+    LabsSettings
+    LabsSharedImage
+    LabsWavefrontMesh
+    Multimedia
+    MultimediaQuick
+    MultimediaWidgets
+    Network
+    Nfc
+    OpenGL
+    OpenGLWidgets
+    Positioning
+    PositioningQuick
+    PrintSupport
+    Qml
+    QmlCompiler
+    QmlCore
+    QmlLocalStorage
+    QmlModels
+    QmlWorkerScript
+    QmlXmlListModel
+    Quick
+    Quick3DSpatialAudio
+    QuickControls2
+    QuickControls2Impl
+    QuickDialogs2
+    QuickDialogs2QuickImpl
+    QuickDialogs2Utils
+    QuickLayouts
+    QuickParticles
+    QuickShapes
+    QuickTemplates2
+    QuickTest
+    QuickWidgets
+    Sensors
+    SensorsQuick
+    ShaderTools
+    SpatialAudio
+    Sql
+    Svg
+    SvgWidgets
+    Test
+    UiTools
+    Widgets
+    Xml
+)
+
+foreach (qt_lib ${qt_libs})
+  if (WIN)
+    install(FILES ${Qt6_base_dir}/bin/Qt6${qt_lib}.dll DESTINATION .)
+  elseif (LNX)
+    install(FILES ${Qt6_base_dir}/lib/libQt6${qt_lib}.so DESTINATION .)
+  endif ()
+endforeach ()
+
+if (WIN)
+  install(FILES ${Qt6_base_dir}/bin/d3dcompiler_47.dll ${Qt6_base_dir}/bin/opengl32sw.dll
+          DESTINATION .
   )
+endif ()
 
-  foreach (qt_lib ${qt_libs})
-    install(FILES ${Qt6_base_dir}/bin/${qt_lib}.dll DESTINATION .)
-  endforeach ()
+# ########################################################################################
+# Translations
+# ########################################################################################
+if (WIN)
+  set(translations_dir "translations")
+elseif (LNX)
+  set(translations_dir "share/translations")
+elseif (MACOS)
+  set(translations_dir "Input.app/Contents/Resources/i18n")
+endif ()
 
-  # Translations
-  install(
-    DIRECTORY app/i18n/
-    DESTINATION translations
-    FILES_MATCHING
-    PATTERN "*.qm"
-  )
+install(
+  DIRECTORY app/i18n/
+  DESTINATION ${translations_dir}
+  FILES_MATCHING
+  PATTERN "*.qm"
+)
 
-  # Assets
+# ########################################################################################
+# Assets
+# ########################################################################################
+if (WIN)
+  set(assets_dir ".")
+elseif (LNX)
+  set(assets_dir "share/input")
+elseif (MACOS)
+  set(assets_dir "Input.app/Contents/Resources/INPUT")
+endif ()
+
+install(DIRECTORY app/android/assets/demo-projects DESTINATION ${assets_dir})
+install(DIRECTORY app/android/assets/qgis-data DESTINATION ${assets_dir})
+
+# ########################################################################################
+# Platform
+# ########################################################################################
+if (WIN)
   install(FILES app/win/AppIcon.ico DESTINATION images/)
   install(FILES app/win/license.txt DESTINATION .)
-  install(DIRECTORY app/android/assets/demo-projects DESTINATION .)
-  install(DIRECTORY app/android/assets/qgis-data DESTINATION .)
-
-else ()
-  set(QT_BIN_DIR ${Qt6_DIR}/../../../bin)
-  if (LNX)
-    set(assets_dir "share/INPUT")
-    set(executable_path "\${QT_DEPLOY_BIN_DIR}/$<TARGET_FILE_NAME:Input>")
-  elseif (MACOS)
-    set(assets_dir "Input.app/Contents/Resources/INPUT")
-    set(executable_path "Input.app")
-  endif ()
-
-  # Binary
-  install(
-    TARGETS Input
-    LIBRARY DESTINATION lib/
-    BUNDLE DESTINATION .
-  )
-
-  # Assets
-  install(DIRECTORY app/android/assets/demo-projects DESTINATION ${assets_dir})
-  install(DIRECTORY app/android/assets/qgis-data DESTINATION ${assets_dir})
-
-  # Use macdeployqt when possible
-  if (MACOS)
-    get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
-    if (isMultiConfig)
-      set(deploy_script "${CMAKE_CURRENT_BINARY_DIR}/deploy_input_$<CONFIG>.cmake")
-    else ()
-      set(deploy_script
-          "${CMAKE_CURRENT_BINARY_DIR}/deploy_input_${CMAKE_BUILD_TYPE}.cmake"
-      )
-    endif ()
-
-    # https://doc-snapshots.qt.io/qt6-dev/qt-deploy-runtime-dependencies.html Replace with
-    # qt_generate_deploy_script from QT 6.5.x The following script must only be executed
-    # at install time Note: This command is in technology preview and may change in future
-    # releases. (QT 6.4.x)
-    file(
-      GENERATE
-      OUTPUT ${deploy_script}
-      CONTENT
-        "
-			# Including the file pointed to by QT_DEPLOY_SUPPORT ensures the generated
-			# deployment script has access to qt_deploy_runtime_dependencies()
-			include(\"${QT_DEPLOY_SUPPORT}\")
-			qt_deploy_runtime_dependencies(
-				EXECUTABLE \"${executable_path}\"
-				GENERATE_QT_CONF
-				VERBOSE
-			)"
-    )
-
-    install(SCRIPT ${deploy_script})
-  endif ()
-
 endif ()
