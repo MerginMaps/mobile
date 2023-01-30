9c7f102e86f4d60bf1ee85c10266e6a83fb13fa8
Alexander Bruy <alexander.bruy@gmail.com>
2023-01-30 15:09:40 +0200

    use raw value for validation

--- app/attributes/fieldvalidator.cpp
+++ app/attributes/fieldvalidator.cpp
@@ -35,7 +35,8 @@ FieldValidator::ValidationStatus FieldValidator::validate( const FeatureLayerPai
   ValidationStatus state = Valid;
 
   const QgsField field = item.field();
-  QVariant value = pair.feature().attribute( item.fieldIndex() );
+  //QVariant value = pair.feature().attribute( item.fieldIndex() );
+  QVariant value = item.rawValue();
 
   bool isNumeric = item.editorWidgetType() == QStringLiteral( "Range" ) || field.isNumeric();
   if ( isNumeric )
