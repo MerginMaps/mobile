a5d85ae65c71ac2aa78047912c76bfbbd1dc20f7
Alexander Bruy <alexander.bruy@gmail.com>
2023-02-02 10:46:25 +0200

    more test fixes

--- app/test/testvariablesmanager.cpp
+++ app/test/testvariablesmanager.cpp
@@ -22,8 +22,9 @@ TestVariablesManager::TestVariablesManager( VariablesManager *vm, PositionKit *p
   mAppSettings = as;
 
   mPositionKit->setAppSettings( mAppSettings );
+  mPositionKit->setPositionProvider( nullptr );
+
   mVariablesManager->setPositionKit( mPositionKit );
-  //QVERIFY( mVariablesManager );
 }
 
 void TestVariablesManager::init()
@@ -38,14 +39,7 @@ void TestVariablesManager::cleanup()
 
 void TestVariablesManager::positionVariables()
 {
-  // Init test data
-  //~ GeoPosition geoInfo = testGeoPosition();
-  //~ bool useGpsPoint = true;
-  //~ double direction = 123.45;
-  //~ double antennaHeight = 3.654;
-  //~ QString providerId = QStringLiteral( "devicegps" );
-  //~ QString providerName = QStringLiteral( "internal" );
-  //~ QString providerType = QStringLiteral( "internal" );
+  mAppSettings->setGpsAntennaHeight(0);
 
   BluetoothPositionProvider *btProvider = new BluetoothPositionProvider( "AA:AA:FF:AA:00:10", "testBluetoothProvider" );
   mPositionKit->setPositionProvider( btProvider );
@@ -56,33 +50,44 @@ void TestVariablesManager::positionVariables()
   fullNmeaFile.open( QFile::ReadOnly );
   QVERIFY( fullNmeaFile.isOpen() );
   QgsGpsInformation position = parser.parseNmeaString( fullNmeaFile.readAll() );
-  emit btProvider->positionChanged( GeoPosition::fromQgsGpsInformation( position ) );
-
-  qDebug() << mPositionKit->latitude();
-  qDebug() << mPositionKit->longitude();
-  qDebug() << mPositionKit->horizontalAccuracy();
+  GeoPosition pos = GeoPosition::fromQgsGpsInformation( position );
+  pos.verticalSpeed = 0;
+  pos.magneticVariation = 0;
+  emit btProvider->positionChanged( pos );
 
   QgsExpressionContext context;
   context << mVariablesManager->positionScope();
-  evaluateExpression( QStringLiteral( "x(@position_coordinate)" ), QStringLiteral( "51.3624998" ), &context );
-  evaluateExpression( QStringLiteral( "y(@position_coordinate)" ), QStringLiteral( "-2.9207148" ), &context );
-  evaluateExpression( QStringLiteral( "@position_latitude" ), QStringLiteral( "-2.9207148" ), &context );
-  evaluateExpression( QStringLiteral( "@position_longitude" ), QStringLiteral( "51.3624998" ), &context );
-  evaluateExpression( QStringLiteral( "@position_altitude" ), QStringLiteral( "0.05" ), &context );
-  evaluateExpression( QStringLiteral( "@position_ground_speed" ), QStringLiteral( "10.34" ), &context );
-  evaluateExpression( QStringLiteral( "@position_vertical_speed" ), QStringLiteral( "11.34" ), &context );
-  evaluateExpression( QStringLiteral( "@position_horizontal_accuracy" ), QStringLiteral( "12.34" ), &context );
-  evaluateExpression( QStringLiteral( "@position_vertical_accuracy" ), QStringLiteral( "13.34" ), &context );
+  evaluateExpression( QStringLiteral( "x(@position_coordinate)" ), QStringLiteral( "17.105920116666667" ), &context );
+  evaluateExpression( QStringLiteral( "y(@position_coordinate)" ), QStringLiteral( "48.103135519999995" ), &context );
+  evaluateExpression( QStringLiteral( "@position_latitude" ), QStringLiteral( "48.103135519999995" ), &context );
+  evaluateExpression( QStringLiteral( "@position_longitude" ), QStringLiteral( "17.105920116666667" ), &context );
+  evaluateExpression( QStringLiteral( "@position_altitude" ), QStringLiteral( "153.026" ), &context );
+  evaluateExpression( QStringLiteral( "@position_ground_speed" ), QStringLiteral( "0.05" ), &context );
+  evaluateExpression( QStringLiteral( "@position_vertical_speed" ), QStringLiteral( "0.00" ), &context );
+  evaluateExpression( QStringLiteral( "@position_horizontal_accuracy" ), QStringLiteral( "0.03" ), &context );
+  evaluateExpression( QStringLiteral( "@position_vertical_accuracy" ), QStringLiteral( "0.04" ), &context );
+  evaluateExpression( QStringLiteral( "@position_magnetic_variation" ), QStringLiteral( "0.00" ), &context );
+  evaluateExpression( QStringLiteral( "@position_direction" ), QStringLiteral( "0" ), &context );
+  evaluateExpression( QStringLiteral( "@position_satellites_visible" ), QStringLiteral( "9" ), &context );
+  evaluateExpression( QStringLiteral( "@position_satellites_used" ), QStringLiteral( "3" ), &context );
+  evaluateExpression( QStringLiteral( "@position_hdop" ), QStringLiteral( "3.20" ), &context );
+  evaluateExpression( QStringLiteral( "@position_gps_fix" ), QStringLiteral( "RTK float" ), &context );
+  evaluateExpression( QStringLiteral( "@position_gps_antenna_height" ), QStringLiteral( "0.000" ), &context );
+  evaluateExpression( QStringLiteral( "@position_provider_address" ), QStringLiteral( "AA:AA:FF:AA:00:10" ), &context );
+  evaluateExpression( QStringLiteral( "@position_provider_name" ), QStringLiteral( "testBluetoothProvider" ), &context );
+  evaluateExpression( QStringLiteral( "@position_provider_type" ), QStringLiteral( "external" ), &context );
+
+  mAppSettings->setGpsAntennaHeight(1.6784);
+  pos.verticalSpeed = 1.345;
+  pos.magneticVariation = 14.34;
+  emit btProvider->positionChanged( pos );
+  context << mVariablesManager->positionScope();
+
+  evaluateExpression( QStringLiteral( "@position_vertical_speed" ), QStringLiteral( "1.34" ), &context );
   evaluateExpression( QStringLiteral( "@position_magnetic_variation" ), QStringLiteral( "14.34" ), &context );
-  evaluateExpression( QStringLiteral( "@position_direction" ), QStringLiteral( "123" ), &context );
-  evaluateExpression( QStringLiteral( "@position_satellites_visible" ), QStringLiteral( "23" ), &context );
-  evaluateExpression( QStringLiteral( "@position_satellites_used" ), QStringLiteral( "21" ), &context );
-  evaluateExpression( QStringLiteral( "@position_hdop" ), QStringLiteral( "1.88" ), &context );
-  evaluateExpression( QStringLiteral( "@position_gps_fix" ), QStringLiteral( "DGPS fix" ), &context );
-  evaluateExpression( QStringLiteral( "@position_gps_antenna_height" ), QStringLiteral( "3.654" ), &context );
-  evaluateExpression( QStringLiteral( "@position_provider_address" ), QStringLiteral( "devicegps" ), &context );
-  evaluateExpression( QStringLiteral( "@position_provider_name" ), QStringLiteral( "internal" ), &context );
-  evaluateExpression( QStringLiteral( "@position_provider_type" ), QStringLiteral( "internal" ), &context );
+  evaluateExpression( QStringLiteral( "@position_gps_antenna_height" ), QStringLiteral( "1.678" ), &context );
+
+  mAppSettings->setGpsAntennaHeight(0);
 }
 
 GeoPosition TestVariablesManager::testGeoPosition()
