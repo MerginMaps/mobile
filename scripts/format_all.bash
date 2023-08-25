a944dc8d489b8b8f3085e632554a695c121d48a3
Peter Petrik <zilolv@gmail.com>
2023-08-25 14:35:27 +0200

    remove qml format

--- scripts/format_all.bash
+++ scripts/format_all.bash
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Script formats both cpp and qml files - intended to be used locally.
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PWD=`pwd`
 cd $DIR
 
 ./format_cmake.bash
 ./format_cpp.bash
-./format_qml.bash
 
 cd $PWD
