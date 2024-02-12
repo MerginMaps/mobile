a48036fec32d0862754925fa7472a65812725405
uclaros <uclaros@gmail.com>
2024-02-12 15:49:57 +0200

    verify file got removed and copied correctly

--- app/test/testmerginapi.cpp
+++ app/test/testmerginapi.cpp
@@ -1106,8 +1106,8 @@ void TestMerginApi::testDiffUpload()
   QVERIFY( !MerginApi::hasLocalProjectChanges( projectDir ) );
 
   // replace gpkg with a new version with a modified geometry
-  QFile::remove( projectDir + "/base.gpkg" );
-  QFile::copy( mTestDataPath + "/modified_1_geom.gpkg", projectDir + "/base.gpkg" );
+  QVERIFY( QFile::remove( projectDir + "/base.gpkg" ) );
+  QVERIFY( QFile::copy( mTestDataPath + "/modified_1_geom.gpkg", projectDir + "/base.gpkg" ) );
 
   ProjectDiff diff = MerginApi::localProjectChanges( projectDir );
   ProjectDiff expectedDiff;
@@ -1143,8 +1143,8 @@ void TestMerginApi::testDiffSubdirsUpload()
   QVERIFY( !MerginApi::hasLocalProjectChanges( projectDir ) );
 
   // replace gpkg with a new version with a modified geometry
-  QFile::remove( projectDir + "/" + base );
-  QFile::copy( mTestDataPath + "/modified_1_geom.gpkg", projectDir + "/" + base );
+  QVERIFY( QFile::remove( projectDir + "/" + base ) );
+  QVERIFY( QFile::copy( mTestDataPath + "/modified_1_geom.gpkg", projectDir + "/" + base ) );
 
   ProjectDiff diff = MerginApi::localProjectChanges( projectDir );
   ProjectDiff expectedDiff;
