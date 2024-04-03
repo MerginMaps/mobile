7a9c1a6a13718e36896619c8fbaef9f5cd6d4c76
Martin Dobias <wonder.sk@gmail.com>
2024-04-03 10:50:19 +0200

    Fix endless loop when failing to log in
    This was caused by the fact we were trying to get user info even
    when login failed, which in turn triggered another login request,
    getting into an endless loop.
    
    Could be triggered by not having network, or by using a wrong server URL.

--- core/merginapi.cpp
+++ core/merginapi.cpp
@@ -87,7 +87,7 @@ MerginApi::MerginApi( LocalProjectsManager &localProjects, QObject *parent )
   QObject::connect( mUserAuth, &MerginUserAuth::authChanged, this, &MerginApi::authChanged );
   QObject::connect( mUserAuth, &MerginUserAuth::authChanged, this, [this]()
   {
-    if ( mUserAuth->hasAuthData() )
+    if ( !mUserAuth->authToken().isEmpty() && mUserAuth->tokenExpiration() >= QDateTime().currentDateTime().toUTC() )
     {
       // do not call /user/profile when user just logged out
       getUserInfo();
