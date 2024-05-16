8a4cd170959c7e765cd4131c765a6c9dc1b33a5d
VitorVieiraZ <vmvieira8@outlook.com>
2024-05-16 13:13:16 -0300

    set android new env variable

--- app/main.cpp
+++ app/main.cpp
@@ -458,6 +458,8 @@ int main( int argc, char *argv[] )
 #ifdef ANDROID
   // See https://bugreports.qt.io/browse/QTBUG-86982 -> fix to make the predictive text disabled on Android
   qputenv( "QT_ANDROID_ENABLE_WORKAROUND_TO_DISABLE_PREDICTIVE_TEXT", "1" );
+  // See issue #3431 -> disable Android accessibility features for the application
+  qputenv("QT_ANDROID_DISABLE_ACCESSIBILITY", "1");
 #endif
 
   // AppSettings has to be initialized after QGIS app init (because of correct reading/writing QSettings).
