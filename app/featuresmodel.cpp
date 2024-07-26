45f294c28b7c41241050b29ec5e755d5cbfa705c
Tomas Mizera <tomas.mizera@lutraconsulting.co.uk>
2024-07-26 15:39:31 +0200

    Fix invalid layer in value relation field (#3542)
    * Fix invalid layer in value relation field
    
    * Fix formatting

--- app/featuresmodel.cpp
+++ app/featuresmodel.cpp
@@ -158,6 +158,12 @@ int FeaturesModel::rowCount( const QModelIndex &parent ) const
 
 QVariant FeaturesModel::featureTitle( const FeatureLayerPair &featurePair ) const
 {
+  if ( !featurePair.layer() || !featurePair.layer()->isValid() )
+  {
+    CoreUtils::log( QStringLiteral( "Features Model" ), QStringLiteral( "Received invalid feature layer pair!" ) );
+    return tr( "Unknown title" );
+  }
+
   QString title;
 
   QgsExpressionContext context( QgsExpressionContextUtils::globalProjectLayerScopes( featurePair.layer() ) );
