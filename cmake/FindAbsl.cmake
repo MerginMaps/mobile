7f032fbef56263ecd7878b56f886fe27ff2740e1
PeterPetrik <zilolv@gmail.com>
2025-03-07 18:16:57 +0100

    absl linux build

--- cmake/FindAbsl.cmake
+++ cmake/FindAbsl.cmake
@@ -1,211 +1,200 @@
 # GPLv2 Licence
 
 set(absl_libs
-    decode_rust_punycode
-    demangle_rust
-    flags_commandlineflag
-    flags_commandlineflag_internal
-    flags_config;flags_internal
-    flags_marshalling;flags_parse
-    flags_private_handle_accessor;flags_program_name
-    flags_reflection;flags_usage
-    flags_usage_internal
-    log_flags;log_internal_structured_proto
-    poison
-    tracing_internal
-    utf8_for_code_point
-    atomic_hook
-    errno_saver
-    log_severity
-    no_destructor
-    nullability
-    raw_logging_internal
-    spinlock_wait
-    config
-    dynamic_annotations
-    core_headers
-    malloc_internal
-    base_internal
-    base
-    throw_delegate
-    pretty_function
-    endian
-    scoped_set_env
-    strerror
-    fast_type_id
-    prefetch
-    poison
-    tracing_internal
     algorithm
     algorithm_container
-    cleanup_internal
-    cleanup
+    any
+    any_invocable
+    atomic_hook
+    bad_any_cast
+    bad_any_cast_impl
+    bad_optional_access
+    bad_variant_access
+    base
+    base_internal
+    bind_front
+    bits
+    bounded_utf8_length_sequence
     btree
-    compressed_tuple
-    fixed_array
-    inlined_vector_internal
-    inlined_vector
-    flat_hash_map
-    flat_hash_set
-    node_hash_map
-    node_hash_set
-    hash_container_defaults
-    container_memory
-    hash_function_defaults
-    hash_policy_traits
+    charset
+    check
+    city
+    civil_time
+    cleanup
+    cleanup_internal
     common_policy_traits
-    hashtablez_sampler
-    hashtable_debug
-    hashtable_debug_hooks
-    node_slot_policy
-    raw_hash_map
+    compare
+    compressed_tuple
+    config
     container_common
-    raw_hash_set
-    layout
+    container_memory
+    cord
+    cord_internal
+    cordz_functions
+    cordz_handle
+    cordz_info
+    cordz_sample_token
+    cordz_statistics
+    cordz_update_scope
+    cordz_update_tracker
+    core_headers
+    crc32c
+    crc_cord_state
     crc_cpu_detect
     crc_internal
-    crc32c
-    non_temporal_arm_intrinsics
-    non_temporal_memcpy
-    crc_cord_state
-    stacktrace
-    symbolize
-    examine_stack
-    failure_signal_handler
-    debugging_internal
-    demangle_internal
-    bounded_utf8_length_sequence
-    decode_rust_punycode
-    demangle_rust
-    utf8_for_code_point
-    leak_check
     debugging
-    flags_path_util
-    flags_program_name
-    flags_config
-    flags_marshalling
-    flags_commandlineflag_internal
-    flags_commandlineflag
-    flags_private_handle_accessor
-    flags_reflection
-    flags_internal
+    debugging_internal
+    decode_rust_punycode
+    demangle_internal
+    demangle_rust
+    die_if_null
+    dynamic_annotations
+    endian
+    errno_saver
+    examine_stack
+    exponential_biased
+    failure_signal_handler
+    fast_type_id
+    fixed_array
     flags
-    flags_usage_internal
-    flags_usage
+    flags_commandlineflag
+    flags_commandlineflag_internal
+    flags_config
+    flags_config;flags_internal
+    flags_internal
+    flags_marshalling
+    flags_marshalling;flags_parse
     flags_parse
-    any_invocable
-    bind_front
+    flags_path_util
+    flags_private_handle_accessor
+    flags_private_handle_accessor;flags_program_name
+    flags_program_name
+    flags_reflection
+    flags_reflection;flags_usage
+    flags_usage
+    flags_usage_internal
+    flat_hash_map
+    flat_hash_set
     function_ref
-    overload
+    graphcycles_internal
+    has_ostream_operator
     hash
-    city
-    low_level_hash
+    hash_container_defaults
+    hash_function_defaults
+    hash_policy_traits
+    hashtable_debug
+    hashtable_debug_hooks
+    hashtablez_sampler
+    if_constexpr
+    inlined_vector
+    inlined_vector_internal
+    int128
+    kernel_timeout_internal
+    layout
+    leak_check
+    log
+    log_entry
+    log_flags
+    log_flags;log_internal_structured_proto
+    log_globals
+    log_initialize
+    log_internal_append_truncated
     log_internal_check_impl
     log_internal_check_op
     log_internal_conditions
     log_internal_config
     log_internal_flags
+    log_internal_fnmatch
     log_internal_format
     log_internal_globals
     log_internal_log_impl
-    log_internal_proto
-    log_internal_message
     log_internal_log_sink_set
+    log_internal_message
     log_internal_nullguard
     log_internal_nullstream
+    log_internal_proto
     log_internal_strip
+    log_internal_structured
+    log_internal_structured_proto
     log_internal_voidify
-    log_internal_append_truncated
-    check
-    log
-    check
-    die_if_null
-    log_flags
-    log_globals
-    log_initialize
-    log
-    log_entry
+    log_severity
     log_sink
     log_sink_registry
     log_streamer
-    log_internal_structured
-    log_internal_structured_proto
     log_structured
-    vlog_config_internal
-    vlog_is_on
-    vlog_is_on
-    log_internal_fnmatch
+    low_level_hash
+    malloc_internal
     memory
-    type_traits
     meta
-    bits
-    int128
+    no_destructor
+    node_hash_map
+    node_hash_set
+    node_slot_policy
+    non_temporal_arm_intrinsics
+    non_temporal_memcpy
+    nullability
     numeric
     numeric_representation
-    sample_recorder
-    exponential_biased
+    optional
+    overload
     periodic_sampler
-    random_random
+    poison
+    prefetch
+    pretty_function
     random_bit_gen_ref
-    random_internal_mock_helpers
     random_distributions
-    random_seed_gen_exception
-    random_seed_sequences
-    random_internal_traits
     random_internal_distribution_caller
+    random_internal_distribution_test_util
     random_internal_fast_uniform_bits
-    random_internal_seed_material
-    random_internal_pool_urbg
-    random_internal_salted_seed_seq
-    random_internal_iostream_state_saver
-    random_internal_generate_real
-    random_internal_wide_multiply
     random_internal_fastmath
+    random_internal_generate_real
+    random_internal_iostream_state_saver
+    random_internal_mock_helpers
     random_internal_nonsecure_base
     random_internal_pcg_engine
-    random_internal_randen_engine
     random_internal_platform
+    random_internal_pool_urbg
     random_internal_randen
-    random_internal_randen_slow
+    random_internal_randen_engine
     random_internal_randen_hwaes
     random_internal_randen_hwaes_impl
-    random_internal_distribution_test_util
+    random_internal_randen_slow
+    random_internal_salted_seed_seq
+    random_internal_seed_material
+    random_internal_traits
     random_internal_uniform_helper
+    random_internal_wide_multiply
+    random_random
+    random_seed_gen_exception
+    random_seed_sequences
+    raw_hash_map
+    raw_hash_set
+    raw_logging_internal
+    sample_recorder
+    scoped_set_env
+    span
+    spinlock_wait
+    stacktrace
     status
     statusor
-    string_view
-    strings
-    charset
-    has_ostream_operator
-    strings_internal
     str_format
     str_format_internal
-    cord_internal
-    cordz_update_tracker
-    cordz_functions
-    cordz_statistics
-    cordz_handle
-    cordz_info
-    cordz_sample_token
-    cordz_update_scope
-    cord
-    graphcycles_internal
-    kernel_timeout_internal
+    strerror
+    string_view
+    strings
+    strings_internal
+    symbolize
     synchronization
+    throw_delegate
     time
-    civil_time
     time_zone
-    any
-    bad_any_cast
-    bad_any_cast_impl
-    span
-    optional
-    bad_optional_access
-    bad_variant_access
-    variant
-    compare
+    tracing_internal
+    type_traits
+    utf8_for_code_point
     utility
-    if_constexpr
+    variant
+    vlog_config_internal
+    vlog_is_on
 )
 
 foreach (absl_lib IN ITEMS ${absl_libs})
