228f77a3e5008d8fedc413583c30898dbc8128d4
PeterPetrik <zilolv@gmail.com>
2025-04-12 13:33:21 +0200

    move mobile-sdk deps to main mobile repo

--- cmake/FindSpatialite.cmake
+++ cmake/FindSpatialite.cmake
@@ -3,15 +3,11 @@
 find_path(
   Spatialite_INCLUDE_DIR
   spatialite.h
-  "${INPUT_SDK_PATH_MULTI}/include"
-  NO_DEFAULT_PATH
 )
 
 find_library(
   Spatialite_LIBRARY
   NAMES spatialite
-  PATHS "${INPUT_SDK_PATH_MULTI}/lib"
-  NO_DEFAULT_PATH
 )
 
 find_package_handle_standard_args(
