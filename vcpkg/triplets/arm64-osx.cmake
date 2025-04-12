c3312b8575660864c1099ff2bf34999e77001f45
PeterPetrik <zilolv@gmail.com>
2025-04-12 13:33:21 +0200

    move mobile-sdk deps to main mobile repo

---
+++ vcpkg/triplets/arm64-osx.cmake
@@ -0,0 +1,16 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES arm64)
+set(VCPKG_BUILD_TYPE release)
+
+if(DEFINED ENV{DEPLOYMENT_TARGET})
+  set(VCPKG_OSX_DEPLOYMENT_TARGET $ENV{DEPLOYMENT_TARGET})
+endif()
+# See https://github.com/microsoft/vcpkg/issues/10038
+set(VCPKG_C_FLAGS -mmacosx-version-min=${VCPKG_OSX_DEPLOYMENT_TARGET})
+set(VCPKG_CXX_FLAGS -mmacosx-version-min=${VCPKG_OSX_DEPLOYMENT_TARGET})
+
+set(VCPKG_ENV_PASSTHROUGH Qt6_DIR) # needed by port/qt6/vcpkg-cmake-wrapper.cmake
