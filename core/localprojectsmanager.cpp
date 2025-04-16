35ca398efbd01e7aa3a55b7a25b359cf09286916
VitorVieiraZ <vmvieira8@outlook.com>
2025-04-16 12:55:56 -0300

    clearing settings on local project removal

--- core/localprojectsmanager.cpp
+++ core/localprojectsmanager.cpp
@@ -115,6 +115,9 @@ void LocalProjectsManager::removeLocalProject( const QString &projectId )
     {
       emit aboutToRemoveLocalProject( mProjects[i] );
 
+      QString projectFullName = mProjects[i].fullName();
+      clearAllProjectSettings( projectFullName );
+
       CoreUtils::removeDir( mProjects[i].projectDir );
       mProjects.removeAt( i );
 
@@ -249,3 +252,17 @@ void LocalProjectsManager::addProject( const QString &projectDir, const QString 
   mProjects << project;
   emit localProjectAdded( project );
 }
+
+void LocalProjectsManager::clearAllProjectSettings( const QString &projectFullName )
+{
+  clearCachedAttributesValueSettings( projectFullName );
+}
+
+void LocalProjectsManager::clearCachedAttributesValueSettings( const QString &projectFullName )
+{
+  QSettings settings;
+  settings.beginGroup( "cachedAttributesValue/" + projectFullName );
+  settings.remove( "" );
+  settings.endGroup();
+}
+
--- core/localprojectsmanager.h
+++ core/localprojectsmanager.h
@@ -68,6 +68,12 @@ class LocalProjectsManager : public QObject
     //! Finds all QGIS project files and set the err variable if any occured.
     QString findQgisProjectFile( const QString &projectDir, QString &err );
 
+    //! Clears all settings groups for the given project full name
+    void clearAllProjectSettings( const QString &projectFullName );
+
+    //! Clears only the cachedAttributesValue settings group for the given project full name
+    void clearCachedAttributesValueSettings( const QString &projectFullName );
+
   signals:
     void localProjectAdded( const LocalProject &project );
     void localProjectDataChanged( const LocalProject &project );
