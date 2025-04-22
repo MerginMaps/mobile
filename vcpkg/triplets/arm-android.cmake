84cc0e198a2131379acaf6f32538b039769163cf
PeterPetrik <zilolv@gmail.com>
2025-04-22 15:24:45 +0200

    use Qt from vcpkg

--- vcpkg/triplets/arm-android.cmake
+++ vcpkg/triplets/arm-android.cmake
@@ -8,13 +8,10 @@ set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=armeabi-v7a -DANDROID_ARM_NEON=O
 
 if(DEFINED ENV{ANDROIDAPI})
   set(VCPKG_CMAKE_SYSTEM_VERSION $ENV{ANDROIDAPI})
+else()
+  message(FATAL_ERROR "ANDROIDAPI not defined as env variable")
 endif()
 
 set(VCPKG_CXX_FLAGS "-fstack-protector-strong")
 set(VCPKG_C_FLAGS "-fstack-protector-strong")
-
 set(VCPKG_LINKER_FLAGS "-lunwind -Wl,--exclude-libs=libunwind.a")
-
-set(VCPKG_ENV_PASSTHROUGH Qt6_DIR) # needed by port/qt6/vcpkg-cmake-wrapper.cmake
-set(VCPKG_ENV_PASSTHROUGH QT_HOST_PATH) # needed by port/qt6/vcpkg-cmake-wrapper.cmake
-set(QT_HOST_PATH $ENV{QT_HOST_PATH} CACHE PATH "needed by QtPublicDependencyHelpers.cmake" FORCE) 
