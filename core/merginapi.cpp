67944dede9a51316e057692222500a98790467a3
Withalion <bagarmatej@gmail.com>
2025-04-22 23:46:02 +0200

    Fix password hint message

--- core/merginapi.cpp
+++ core/merginapi.cpp
@@ -829,16 +829,15 @@ void MerginApi::registerUser( const QString &email,
 
   if ( password.isEmpty() || password.length() < 8 )
   {
-    QString msg = tr( "Password not strong enough!%1"
-                      "%3 Password must be at least 8 characters long.%4"
-                      "%3 Password must contain at least 3 character categories among the following:"
-                      "<ul type=\"disc\">"
-                      "%3 Lowercase characters (a-z)%4"
-                      "%3 Uppercase characters (A-Z)%4"
-                      "%3 Digits (0-9)%4"
-                      "%3 Special characters %4"
-                      "%2%4%2" )
-                  .arg( "<ul>", "</ul>", "<li>", "</li>" );
+    const QString msg = tr( "%1%3 Password must be at least 8 characters long.%4"
+                            "%3 Include at least 3 character categories among the following:"
+                            "<ul type=\"disc\">"
+                            "%3 Lowercase characters (a-z)%4"
+                            "%3 Uppercase characters (A-Z)%4"
+                            "%3 Digits (0-9)%4"
+                            "%3 Special characters %4"
+                            "%2%4%2" )
+                        .arg( "<ul>", "</ul>", "<li>", "</li>" );
     emit registrationFailed( msg, RegistrationError::RegistrationErrorType::PASSWORD );
     return;
 
@@ -1319,9 +1318,8 @@ void MerginApi::registrationFinished( const QString &login, const QString &passw
       }
       else if ( serverErrorCode == "InvalidPassword" )
       {
-        const QString msg = tr( "Password not strong enough!%1"
-                                "%3 Password must be at least 8 characters long.%4"
-                                "%3 Password must contain at least 3 character categories among the following:"
+        const QString msg = tr( "%1%3 Password must be at least 8 characters long.%4"
+                                "%3 Include at least 3 character categories among the following:"
                                 "<ul type=\"disc\">"
                                 "%3 Lowercase characters (a-z)%4"
                                 "%3 Uppercase characters (A-Z)%4"
