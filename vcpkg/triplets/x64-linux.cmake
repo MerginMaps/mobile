440120e20de22776a2ba8680f0ea91e9426fa624
PeterPetrik <zilolv@gmail.com>
2025-04-25 08:59:13 +0200

    try fix linux?

--- vcpkg/triplets/x64-linux.cmake
+++ vcpkg/triplets/x64-linux.cmake
@@ -1,6 +1,12 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
+
+include("${CMAKE_CURRENT_LIST_DIR}/static-ports.cmake")
+if (PORT IN_LIST STATIC_PORTS)
+    set(VCPKG_LIBRARY_LINKAGE static)
+else()
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_BUILD_TYPE release)
