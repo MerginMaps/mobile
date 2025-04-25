6f7c185c94b09e6bd14f334f4757abbacda2a54c
PeterPetrik <zilolv@gmail.com>
2025-04-25 10:50:31 +0200

    try android

--- vcpkg/triplets/arm64-android.cmake
+++ vcpkg/triplets/arm64-android.cmake
@@ -3,7 +3,12 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
-set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
+
+
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-darwin-android")
+# TODO linux
+# set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
+
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=arm64-v8a)
 
 set(VCPKG_CXX_FLAGS "-fstack-protector-strong")
@@ -11,8 +16,7 @@ set(VCPKG_C_FLAGS "-fstack-protector-strong")
 
 if(DEFINED ENV{ANDROIDAPI})
   set(VCPKG_CMAKE_SYSTEM_VERSION $ENV{ANDROIDAPI})
+else()
+    message(FATAL_ERROR "$ENV{ANDROIDAPI} not defined as env variable")
 endif()
 
-set(VCPKG_ENV_PASSTHROUGH Qt6_DIR) # needed by port/qt6/vcpkg-cmake-wrapper.cmake
-set(VCPKG_ENV_PASSTHROUGH QT_HOST_PATH) # needed by port/qt6/vcpkg-cmake-wrapper.cmake
-set(QT_HOST_PATH $ENV{QT_HOST_PATH} CACHE PATH "needed by QtPublicDependencyHelpers.cmake" FORCE) 
