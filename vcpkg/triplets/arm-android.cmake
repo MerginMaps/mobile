830436c59f16a7c9d89060c68865a1a7ffe9a2d6
PeterPetrik <zilolv@gmail.com>
2025-04-25 10:50:31 +0200

    try android

--- vcpkg/triplets/arm-android.cmake
+++ vcpkg/triplets/arm-android.cmake
@@ -3,7 +3,12 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
-set(VCPKG_MAKE_BUILD_TRIPLET "--host=armv7a-linux-androideabi")
+
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=armv7a-darwin-androideabi")
+# TODO linux
+# set(VCPKG_MAKE_BUILD_TRIPLET "--host=armv7a-linux-androideabi")
+
+
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=armeabi-v7a -DANDROID_ARM_NEON=ON)
 
 if(DEFINED ENV{ANDROIDAPI})
