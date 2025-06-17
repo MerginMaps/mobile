695be8a7a32dcb89dc051c886336affb5ba1295c
Withalion <bagarmatej@gmail.com>
2025-06-17 15:27:09 +0200

    Change photo sketches to be opt-in

--- app/activeproject.cpp
+++ app/activeproject.cpp
@@ -625,5 +625,5 @@ bool ActiveProject::photoSketchesEnabled() const
     return false;
   }
 
-  return mQgsProject->readBoolEntry( QStringLiteral( "Mergin" ), QStringLiteral( "PhotoSketches/Enabled" ), true );
+  return mQgsProject->readBoolEntry( QStringLiteral( "Mergin" ), QStringLiteral( "PhotoSketches/Enabled" ), false );
 }
