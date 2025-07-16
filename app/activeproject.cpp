2b5555a0adbfa0a8232a55200dd0bd3b3512e580
Withalion <bagarmatej@gmail.com>
2025-07-16 15:06:21 +0300

    Fix review issues

--- app/activeproject.cpp
+++ app/activeproject.cpp
@@ -625,5 +625,5 @@ bool ActiveProject::photoSketchingEnabled() const
     return false;
   }
 
-  return mQgsProject->readBoolEntry( QStringLiteral( "Mergin" ), QStringLiteral( "PhotoSketches/Enabled" ), false );
+  return mQgsProject->readBoolEntry( QStringLiteral( "Mergin" ), QStringLiteral( "PhotoSketching/Enabled" ), true );
 }
