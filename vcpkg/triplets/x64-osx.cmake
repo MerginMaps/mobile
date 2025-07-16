ad3ff7b485b3522528969e82436af82846a319bb
Matej Bagar <matej.bagar@lutranconsulting.co.uk>
2025-07-16 20:04:58 +0200

    Fix issues with ios build

--- vcpkg/triplets/x64-osx.cmake
+++ vcpkg/triplets/x64-osx.cmake
@@ -6,11 +6,8 @@ set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
 set(VCPKG_BUILD_TYPE release)
 
-if(DEFINED ENV{DEPLOYMENT_TARGET})
-  set(VCPKG_OSX_DEPLOYMENT_TARGET $ENV{DEPLOYMENT_TARGET})
-else()
-    message(FATAL_ERROR "DEPLOYMENT_TARGET not defined as env variable")
-endif()
+set(VCPKG_OSX_DEPLOYMENT_TARGET 11.0)
+
 # See https://github.com/microsoft/vcpkg/issues/10038
 set(VCPKG_C_FLAGS -mmacosx-version-min=${VCPKG_OSX_DEPLOYMENT_TARGET})
 set(VCPKG_CXX_FLAGS -mmacosx-version-min=${VCPKG_OSX_DEPLOYMENT_TARGET})
