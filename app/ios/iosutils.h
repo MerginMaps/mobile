c0cfd1e856c211a53a3b74215e9b1644ebce91af
Withalion <bagarmatej@gmail.com>
2025-07-17 22:55:21 +0300

    Fix android build

--- app/ios/iosutils.h
+++ app/ios/iosutils.h
@@ -49,7 +49,10 @@ class IosUtils: public QObject
 
     static Q_INVOKABLE QString getManufacturer();
     static Q_INVOKABLE QString getDeviceModel();
+
+    #ifdef Q_OS_IOS
     static Q_INVOKABLE void vibrate();
+    #endif
 
   signals:
     void imageSelected( const QString &imagePath, const QString &code );
