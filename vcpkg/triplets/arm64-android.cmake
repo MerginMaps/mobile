eb17ba4601ca72a3e3584fed4c6f5697ab10ce28
PeterPetrik <zilolv@gmail.com>
2025-07-29 08:54:49 +0200

    more work on android

--- vcpkg/triplets/arm64-android.cmake
+++ vcpkg/triplets/arm64-android.cmake
@@ -9,12 +9,11 @@ set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
 
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=arm64-v8a)
 
+set(ANDROIDAPI "28") # when changed change CMakeLists.cmake
+set(ANDROID_PLATFORM "android-${ANDROIDAPI}")
+set(ANDROID_NDK_PLATFORM "android-${ANDROIDAPI}")
+set(VCPKG_CMAKE_SYSTEM_VERSION "${ANDROIDAPI}")
+set(ANDROID_STL "c++_shared")
+
 set(VCPKG_CXX_FLAGS "-fstack-protector-strong")
 set(VCPKG_C_FLAGS "-fstack-protector-strong")
-
-if(DEFINED ENV{ANDROIDAPI})
-  set(VCPKG_CMAKE_SYSTEM_VERSION $ENV{ANDROIDAPI})
-else()
-    message(FATAL_ERROR "$ENV{ANDROIDAPI} not defined as env variable")
-endif()
-
