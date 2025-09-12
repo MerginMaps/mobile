f32b5a582e602ef7979398d2255b91e4820e82f6
Matej Bagar <matej.bagar@lutraconsulting.co.uk>
2025-09-12 11:30:32 +0200

    Rename input cmake variables

--- cmake/VersionDetect.cmake
+++ cmake/VersionDetect.cmake
@@ -2,15 +2,15 @@
 
 # needs to be called after project() command so toolchains are already evaluated
 macro (mm_detect_version)
-  set(INPUT_VERSION ${MM_VERSION_MAJOR}.${MM_VERSION_MINOR}.${MM_VERSION_PATCH})
-  set(VERSTR ${INPUT_VERSION})
+  set(MM_VERSION ${MM_VERSION_MAJOR}.${MM_VERSION_MINOR}.${MM_VERSION_PATCH})
+  set(VERSTR ${MM_VERSION})
 
   string(
     REPLACE " "
             "\ "
             ESCAPED_VERSTR
-            ${INPUT_VERSION}
+            ${MM_VERSION}
   )
 
-  set(version_desc "version name ${INPUT_VERSION}; code ${INPUT_VERSION_CODE}")
+  set(version_desc "version name ${MM_VERSION}; code ${MM_VERSION_CODE}")
 endmacro ()
