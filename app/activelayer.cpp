5233b03ed46f99f42a00245ee625ef6ab4ffcaa0
Tomas Mizera <tomas.mizera@lutraconsulting.co.uk>
2025-09-19 13:18:01 +0200

    trugger tests

--- app/activelayer.cpp
+++ app/activelayer.cpp
@@ -6,7 +6,7 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-
+  
 
 #include "activelayer.h"
 
