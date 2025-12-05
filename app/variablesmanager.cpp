1957130aef7d397506f018071f663f609ba22731
Matej Bagar <matej.bagar@lutraconsulting.co.uk>
2025-12-05 10:35:10 +0100

    Add ellipsoidal elevation expr variable

--- app/variablesmanager.cpp
+++ app/variablesmanager.cpp
@@ -72,6 +72,7 @@ QgsExpressionContextScope *VariablesManager::positionScope()
   addPositionVariable( scope, QStringLiteral( "longitude" ), position.longitude );
   addPositionVariable( scope, QStringLiteral( "latitude" ), position.latitude );
   addPositionVariable( scope, QStringLiteral( "altitude" ), position.elevation - mPositionKit->antennaHeight() );
+  addPositionVariable( scope, QStringLiteral( "altitude_ellipsoidal" ), position.elevation - position.elevation_diff - mPositionKit->antennaHeight() );
   addPositionVariable( scope, QStringLiteral( "geoid_separation" ), position.elevation_diff );
   addPositionVariable( scope, QStringLiteral( "horizontal_accuracy" ), getGeoPositionAttribute( position.hacc ) );
   addPositionVariable( scope, QStringLiteral( "vertical_accuracy" ), getGeoPositionAttribute( position.vacc ) );
