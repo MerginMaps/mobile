4f0d2d33b4be8c0258653d937f1aa96cd3e71f57
Matej Bagar <matej.bagar@lutraconsulting.co.uk>
2025-12-05 14:04:23 +0100

    Fix formatting

--- app/inpututils.h
+++ app/inpututils.h
@@ -637,8 +637,8 @@ class InputUtils: public QObject
      * Splits path into components and sanitizes each component using sanitizeFileName().
      */
     static void sanitizePath( QString &path );
-    
-    /**    
+
+    /**
      * Returns the indexes of fields that are referenced in an expression
      */
     Q_INVOKABLE static QSet<int> referencedAttributeIndexes( QgsVectorLayer *layer, const QString &expression );
