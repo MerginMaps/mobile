18b58a415218fdf1f4dc8465e11f4aacab039b17
Matej Bagar <matej.bagar@lutraconsulting.co.uk>
2026-01-30 15:39:50 +0100

    Fix UDP issues

--- app/position/providers/networkpositionprovider.h
+++ app/position/providers/networkpositionprovider.h
@@ -22,11 +22,12 @@ class NetworkPositionProvider : public AbstractPositionProvider
 {
     Q_OBJECT
 
-// signalizes in how many [ms] we will try to reconnect to GPS again
+    // signalizes in how many [ms] we will try to reconnect to GPS again
     enum ReconnectDelay
     {
-      ShortDelay = 3000,
-      LongDelay = 5000
+      ShortDelay = 3000,      // 3 secs
+      LongDelay = 5000,       // 5 secs
+      ExtraLongDelay = 10000  // 10 secs
     };
 
   public:
@@ -37,14 +38,15 @@ class NetworkPositionProvider : public AbstractPositionProvider
     void closeProvider() override;
 
   public slots:
+    // processes the received nmea data and emits new position
     void positionUpdateReceived();
+    // changes the provider state depending on the socket states
     void socketStateChanged( QAbstractSocket::SocketState state );
+    // checks if enough time passed since last reconnect and triggers it if necessary
     void reconnectTimeout();
 
   private:
-// utility function, which should return the socket in use
-    QAbstractSocket *activeSocket() const;
-// trigger the reconnection flow for both sockets
+    // trigger the reconnection flow for both sockets
     void reconnect();
     // start the reconnection timeout
     void startReconnectTimer();
@@ -55,6 +57,7 @@ class NetworkPositionProvider : public AbstractPositionProvider
     int mReconnectDelay = ReconnectDelay::ShortDelay; // in how many [ms] we will try to reconnect again
     int mSecondsLeftToReconnect; // how many seconds are left to reconnect. Reconnects if less than or equal to one
     QTimer mReconnectTimer; // timer that times out each second and lowers the mSecondsLeftToReconnect by one
+    QTimer mUdpReconnectTimer; // timer that times out after ExtraLongDelay and triggers reconnect
 
     QString mTargetAddress; // IP address or hostname of the receiver
     int mTargetPort; // active port of the receiver
