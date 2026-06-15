23b3ed1cbdfa051d24c62d65187263db2c321dda
Stefanos Natsis <uclaros@gmail.com>
2026-06-15 17:59:35 +0300

    Easier switching between qgis build types. (#4529)
    Using QGIS_BUILD_TYPE environment variable the build type of qgis dependency
    can be selected. Valid values are Debug and RelWithDebInfo. Any other values
    or an unset variable will result in using Release build.
    Setting the variable and running cmake should trigger a rebuild of only the
    qgis dependency, or fetching it from the cache if it has been built
    previously.

--- vcpkg/triplets/x64-windows.cmake
+++ vcpkg/triplets/x64-windows.cmake
@@ -2,3 +2,5 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_BUILD_TYPE release)
+
+include("${CMAKE_CURRENT_LIST_DIR}/qgis-build-type.cmake")
